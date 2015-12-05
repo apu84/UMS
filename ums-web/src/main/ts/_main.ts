4a6196f86585ce6b979cbc2ed9dd0e735905f683
apu84 <apu.sust@gmail.com>
2015-12-05 10:21:27 +0600

    Added jquery.validate.d.ts

--- ums-web/src/main/ts/_main.ts
+++ ums-web/src/main/ts/_main.ts
@@ -1,6 +1,7 @@
 /// <reference path='lib/angular.d.ts'/>
 /// <reference path='lib/angular-route.d.ts'/>
 /// <reference path='lib/jquery.d.ts'/>
+/// <reference path='lib/jquery.validate.d.ts'/>
 /// <reference path='lib/jqueryui.d.ts'/>
 /// <reference path='lib/jquery.slimScroll.d.ts'/>
 /// <reference path='lib/zabuto_calendar.d.ts'/>
--- ums-web/src/main/ts/controller/NewSemesterController.ts
+++ ums-web/src/main/ts/controller/NewSemesterController.ts
@@ -1,7 +1,6 @@
-/// <reference path="../../webapp/iums/vendors/jquery-validate/jquery.validate.min.js" />
 module ums {
     UMS.controller('NewSemesterController', function ($scope, $routeParams){
-        $(".form-validate") .validate({
+        $(".form-validate").validate({
             errorPlacement: function(error, element){
                 error.insertAfter(element);
             }
