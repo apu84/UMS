2af181a7112510e71991110d124efaaccaae5aa7
apu84 <apu.sust@gmail.com>
2015-12-29 00:32:45 +0600

    Type issue fixed for SyllabusManager

--- ums-api/src/main/java/org/ums/manager/SyllabusManager.java
+++ ums-api/src/main/java/org/ums/manager/SyllabusManager.java
@@ -1,6 +1,7 @@
 package org.ums.manager;
 
 import org.ums.domain.model.MutableSemester;
+import org.ums.domain.model.MutableSyllabus;
 import org.ums.domain.model.Syllabus;
 
 import java.util.List;
@@ -8,6 +9,6 @@ import java.util.List;
 /**
  * Created by Ifti on 28-Dec-15.
  */
-public interface SyllabusManager extends ContentManager<Syllabus, MutableSemester, Integer> {
+public interface SyllabusManager extends ContentManager<Syllabus, MutableSyllabus, String> {
   public List<Syllabus> getSyllabusList(final Integer pProgramId) throws Exception;
 }
