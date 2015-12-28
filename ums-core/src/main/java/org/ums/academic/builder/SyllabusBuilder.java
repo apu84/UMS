98f322f6d4c21e2537ede61aa6817144f0fd5282
ifti24 <ifticse_kuet@hotmail.com>
2015-12-29 03:38:00 +0600

    Fetched Syllabus List inside Course Create Form.

--- ums-core/src/main/java/org/ums/academic/builder/SyllabusBuilder.java
+++ ums-core/src/main/java/org/ums/academic/builder/SyllabusBuilder.java
@@ -36,7 +36,6 @@ public class SyllabusBuilder implements Builder<Syllabus, MutableSyllabus> {
         .path(String.valueOf(program.getId())).build().toString());
     pBuilder.add("program_name", program.getShortName());
 
-
     pBuilder.add("self", pUriInfo.getBaseUriBuilder().path("academic").path("syllabus")
         .path(pReadOnly.getId()).build().toString());
   }
