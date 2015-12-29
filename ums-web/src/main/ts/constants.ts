b31aa656710597f24e9d05f41b2bfdad2610e2d5
Md. Moshlehuddin Mazumder <apu@escenic.com>
2015-12-29 16:15:25 +0600

    Created initial model for ProgramSelector

--- ums-web/src/main/ts/constants.ts
+++ ums-web/src/main/ts/constants.ts
@@ -2,53 +2,53 @@ module ums {
   export class Constants {
     static  Default():any {
       return {
-        courseType:[
-          {id:'',name:'Select Course Type'},
-          {id:'1',name:'Theory'},
-          {id:'2',name:'Sessional'},
-          {id:'3',name:'Thesis/Project'}
+        courseType: [
+          {id: '', name: 'Select Course Type'},
+          {id: '1', name: 'Theory'},
+          {id: '2', name: 'Sessional'},
+          {id: '3', name: 'Thesis/Project'}
         ],
-        courseCategory:[
-          {id:'',name:'Select Course Category'},
-          {id:'1',name:'Mandatory'},
-          {id:'2',name:'Optional'}
+        courseCategory: [
+          {id: '', name: 'Select Course Category'},
+          {id: '1', name: 'Mandatory'},
+          {id: '2', name: 'Optional'}
         ],
-        academicYear:[
-          {id:'',name:'Select Year'},
-          {id:'1',name:'1st Year'},
-          {id:'2',name:'2nd Year'},
-          {id:'3',name:'3rd Year'},
-          {id:'4',name:'4th Year'}
+        academicYear: [
+          {id: '', name: 'Select Year'},
+          {id: '1', name: '1st Year'},
+          {id: '2', name: '2nd Year'},
+          {id: '3', name: '3rd Year'},
+          {id: '4', name: '4th Year'}
         ],
-        academicSemester:[
-          {id:'',name:'Select Semester'},
-          {id:'1',name:'1st Semester'},
-          {id:'2',name:'2nd Semester'}
+        academicSemester: [
+          {id: '', name: 'Select Semester'},
+          {id: '1', name: '1st Semester'},
+          {id: '2', name: '2nd Semester'}
         ],
-        semester:[
-          {id:'',name:'Select Semester'},
-          {id:'01',name:'Spring'},
-          {id:'02',name:'Fall'}
+        semester: [
+          {id: '', name: 'Select Semester'},
+          {id: '01', name: 'Spring'},
+          {id: '02', name: 'Fall'}
         ],
-        gender:  [
+        gender: [
           {id: '', name: 'Select Gender'},
           {id: 'M', name: 'Male'},
           {id: 'F', name: 'Female'}
         ],
         programType: [
           {id: '', name: 'Select Program Type'},
-          {id:'11', name:'Undergraduate'},
-          {id:'22', name: 'Postgraduate'}
+          {id: '11', name: 'Undergraduate'},
+          {id: '22', name: 'Postgraduate'}
         ],
         deptShort: [
-          {id: '', name :'Select Dept./School'},
-          {id: '01', name :'ARC'},
-          {id: '02',name :'BBA'},
-         {id: '03',name : 'CE'},
-         {id: '04', name :'CSE'},
-         {id:'05',name :'EEE'},
-         {id:'06', name :'TE'},
-         {id:'07', name :'MPE'}
+          {id: '', name: 'Select Dept./School'},
+          {id: '01', name: 'ARC'},
+          {id: '02', name: 'BBA'},
+          {id: '03', name: 'CE'},
+          {id: '04', name: 'CSE'},
+          {id: '05', name: 'EEE'},
+          {id: '06', name: 'TE'},
+          {id: '07', name: 'MPE'}
         ],
         deptLong: {
           '01': 'Department of Architecture',
@@ -79,7 +79,7 @@ module ums {
           '0': 'Inactive',
           '1': 'Active'
         },
-        bloodGroup:  [
+        bloodGroup: [
           {id: '', name: 'Select Blood Group'},
           {id: 'O+', name: 'O+'},
           {id: 'O–', name: 'O-'},
@@ -90,95 +90,100 @@ module ums {
           {id: 'AB+', name: 'AB+'},
           {id: 'AB–', name: 'AB-'}
         ],
-        ugDept:[
+        ugDept: [
           {id: '', name: 'Select Dept./School'},
-          {id: '01', name :'ARC'},
-          {id: '02',name :'BBA'},
-          {id: '03',name : 'CE'},
-          {id: '04', name :'CSE'},
-          {id:'05',name :'EEE'},
-          {id:'06', name :'TE'},
-          {id:'07', name :'MPE'}
+          {id: '01', name: 'ARC'},
+          {id: '02', name: 'BBA'},
+          {id: '03', name: 'CE'},
+          {id: '04', name: 'CSE'},
+          {id: '05', name: 'EEE'},
+          {id: '06', name: 'TE'},
+          {id: '07', name: 'MPE'}
         ],
-        pgDept:[
+        pgDept: [
           {id: '', name: 'Select Dept./School'},
-          {id: '01', name :'ARC'},
-          {id: '02',name :'BBA'},
-          {id:'05',name :'EEE'}
+          {id: '01', name: 'ARC'},
+          {id: '02', name: 'BBA'},
+          {id: '05', name: 'EEE'}
         ],
-        ugPrograms : [{
-          deptId:'01',
-          programs: [
-            {
-              id: "110100",
-              shortName: "BSC in ARC",
-              longName: "Bachelor in Arch. Engineering"
-            }
-          ]
-        },
-        {
-          deptId:'02',
-          programs: [
-            {
-              id: "110200",
-              shortName: "BBA",
-              longName: "Bachelor in Business Administration"
-            }
-          ]
-        },
+        ugPrograms: [
           {
-          deptId: "03",
-          programs: [
-            {
-              id: "110300",
-              shortName: "BSC in CE",
-              longName: "Bachelor in Civil Engineering"
-            }
-          ]
+            deptId: '01',
+            programs: [
+              {
+                id: "110100",
+                shortName: "BSC in ARC",
+                longName: "Bachelor in Arch. Engineering"
+              }
+            ]
           },
           {
-          deptId: "04",
-          programs: [
-            {
-              id: "110400",
-              shortName: "BSC in CSE",
-              longName: "Bachelor in Computer Science and Engineering"
-            }
-          ]
-      },{
-          deptId: "05",
-          programs: [
-            {
-              id: "110500",
-              shortName: "BSC in EEE",
-              longName: "Bachelor in Electrical and Electronic Engineering"
-            }
-          ]
-      },{
-          deptId: "06",
-          programs:[
-            {
-              id: "110600",
-              shortName: "BSC in TE",
-              longName: "Bachelor in Textile Engineering"
-            }
-          ]},
-      {
-          deptId: "07",
-          programs: [
-            {
-              id: "110707",
-              shortName: "BSC in IPE",
-              longName: "Bachelor in Industrial and Production Engineering"
-            },
-            {
-              id: "110708",
-              shortName: "BSC in ME",
-              longName: "Bachelor in Mechanical Engineering"
-            }
-          ]
-        }
-    ]
+            deptId: '02',
+            programs: [
+              {
+                id: "110200",
+                shortName: "BBA",
+                longName: "Bachelor in Business Administration"
+              }
+            ]
+          },
+          {
+            deptId: "03",
+            programs: [
+              {
+                id: "110300",
+                shortName: "BSC in CE",
+                longName: "Bachelor in Civil Engineering"
+              }
+            ]
+          },
+          {
+            deptId: "04",
+            programs: [
+              {
+                id: "110400",
+                shortName: "BSC in CSE",
+                longName: "Bachelor in Computer Science and Engineering"
+              }
+            ]
+          },
+          {
+            deptId: "05",
+            programs: [
+              {
+                id: "110500",
+                shortName: "BSC in EEE",
+                longName: "Bachelor in Electrical and Electronic Engineering"
+              }
+            ]
+          },
+          {
+            deptId: "06",
+            programs: [
+              {
+                id: "110600",
+                shortName: "BSC in TE",
+                longName: "Bachelor in Textile Engineering"
+              }
+            ]
+          },
+          {
+            deptId: "07",
+            programs: [
+              {
+                id: "110707",
+                shortName: "BSC in IPE",
+                longName: "Bachelor in Industrial and Production Engineering"
+              },
+              {
+                id: "110708",
+                shortName: "BSC in ME",
+                longName: "Bachelor in Mechanical Engineering"
+              }
+            ]
+          }
+        ],
+        mimeTypeJson : 'application/json'
       };
     }
   }
