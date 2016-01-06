eb367a57d444e5a2faa3ad65e9a13a89bf310891
Md. Moshlehuddin Mazumder <apu@escenic.com>
2016-01-06 14:29:40 +0600

    Added initial implementation for managing transaction using spring tx

--- ums-webservice-common/src/main/java/org/ums/common/academic/resource/helper/StudentResourceHelper.java
+++ ums-webservice-common/src/main/java/org/ums/common/academic/resource/helper/StudentResourceHelper.java
@@ -3,6 +3,9 @@ package org.ums.common.academic.resource.helper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.ums.academic.builder.Builder;
 import org.ums.academic.model.PersistentStudent;
 import org.ums.academic.model.PersistentUser;
@@ -17,7 +20,6 @@ import javax.json.JsonObject;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Base64;
@@ -39,40 +41,55 @@ public class StudentResourceHelper extends ResourceHelper<Student, MutableStuden
   @Autowired
   private List<Builder<Student, MutableStudent>> mBuilders;
 
+  @Autowired
+  private TransactionTemplate mTransactionTemplate;
+
   @Override
   public Response post(JsonObject pJsonObject, UriInfo pUriInfo) throws Exception {
     //TODO: Three separate task done here; new student, student photo and new user, needs to be bundled with in a transaction
-    MutableStudent mutableStudent = new PersistentStudent();
-    LocalCache localCache = new LocalCache();
-    for (Builder<Student, MutableStudent> builder : mBuilders) {
-      builder.build(mutableStudent, pJsonObject, localCache);
-    }
-    mutableStudent.commit(false);
 
-    try {
-      String encodingPrefix = "base64,", data = pJsonObject.getString("imageData");
-      int contentStartIndex = data.indexOf(encodingPrefix) + encodingPrefix.length();
-      byte[] imageData = Base64.getDecoder().decode(data.substring(contentStartIndex));
-      mBinaryContentManager.create(imageData, pJsonObject.getString("id"), BinaryContentManager.Domain.PICTURE);
+    return mTransactionTemplate.execute(new TransactionCallback<Response>() {
+      Response.ResponseBuilder builder = null;
 
-    } catch (IOException e) {
-      throw new WebApplicationException("Error while uploading file. Please try again !!");
-    }
+      @Override
+      public Response doInTransaction(TransactionStatus status) {
+        try {
+          MutableStudent mutableStudent = new PersistentStudent();
+          LocalCache localCache = new LocalCache();
+          for (Builder<Student, MutableStudent> builder : mBuilders) {
+            builder.build(mutableStudent, pJsonObject, localCache);
+          }
+          mutableStudent.commit(false);
 
-    MutableUser studentUser = new PersistentUser();
-    studentUser.setId(pJsonObject.getString("id"));
-    //TODO: Use a password generator to generate temporary password
-    studentUser.setTemporaryPassword("testPassword".toCharArray());
-    //TODO: Use role name to fetch a particular role, say for Student it should be "student"
-    studentUser.setRole(mRoleManager.get(11));
-    studentUser.setActive(true);
-    studentUser.commit(false);
+          try {
+            String encodingPrefix = "base64,", data = pJsonObject.getString("imageData");
+            int contentStartIndex = data.indexOf(encodingPrefix) + encodingPrefix.length();
+            byte[] imageData = Base64.getDecoder().decode(data.substring(contentStartIndex));
+            mBinaryContentManager.create(imageData, pJsonObject.getString("id"), BinaryContentManager.Domain.PICTURE);
 
-    URI contextURI = pUriInfo.getBaseUriBuilder().path(StudentResource.class).path(StudentResource.class, "get").build(mutableStudent.getId());
-    Response.ResponseBuilder builder = Response.created(contextURI);
-    builder.status(Response.Status.CREATED);
+          } catch (IOException e) {
+            throw new WebApplicationException("Error while uploading file. Please try again !!");
+          }
 
-    return builder.build();
+          MutableUser studentUser = new PersistentUser();
+          studentUser.setId(pJsonObject.getString("id"));
+          //TODO: Use a password generator to generate temporary password
+          studentUser.setTemporaryPassword("testPassword".toCharArray());
+          //TODO: Use role name to fetch a particular role, say for Student it should be "student"
+          studentUser.setRole(mRoleManager.get(11));
+          studentUser.setActive(true);
+          studentUser.commit(false);
+
+          URI contextURI = pUriInfo.getBaseUriBuilder().path(StudentResource.class).path(StudentResource.class, "get").build(mutableStudent.getId());
+          builder = Response.created(contextURI);
+          builder.status(Response.Status.CREATED);
+
+        } catch (Exception e) {
+          throw new WebApplicationException("Exception while creating new student", e);
+        }
+        return builder == null ? null : builder.build();
+      }
+    });
   }
 
   @Override
