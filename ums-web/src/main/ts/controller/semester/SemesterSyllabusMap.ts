906ca1c005d4cd14538fd0880cc1a30e3bb4182b
ifti24 <ifticse_kuet@hotmail.com>
2016-01-11 00:28:54 +0600

    Ajax Loading for S-S Mapping.

--- ums-web/src/main/ts/controller/semester/SemesterSyllabusMap.ts
+++ ums-web/src/main/ts/controller/semester/SemesterSyllabusMap.ts
@@ -12,6 +12,11 @@ module ums {
     maps:any;
     single:any;
     syllabusOptions: any;
+    loadingVisibility1:boolean;
+    loadingVisibility2:boolean;
+    mapTableVisibility:boolean;
+    mapDetailVisiblity:boolean;
+
   }
   export class SemesterSyllabusMap {
     public static $inject = ['appConstants', '$scope', 'HttpClient'];
@@ -23,27 +28,40 @@ module ums {
       $scope.goNext= this.goNext.bind(this);
       $scope.fetchSSmap= this.fetchSSmap.bind(this);
 
+      $scope.mapTableVisibility=false;
+
     }
 
     private goNext():void {
-
+      this.$scope.loadingVisibility1=true;
+      this.$scope.mapTableVisibility=false;
+      this.$scope.mapDetailVisiblity=false;
         this.httpClient.get('academic/ssmap/program/110500/semester/11012015', 'application/json',
             (json:any, etag:string) => {
+              this.$scope.loadingVisibility1=false;
               this.$scope.maps = json.entries;
+              this.$scope.mapTableVisibility=true;
             },
             (response:ng.IHttpPromiseCallbackArg<any>) => {
               console.error(response);
+              this.$scope.loadingVisibility1=false;
             });
 
 
     }
 
     private fetchSSmap(mapId):void {
+      this.$scope.loadingVisibility2=true;
+      this.$scope.mapDetailVisiblity=false;
+
       this.httpClient.get('academic/ssmap/'+mapId, 'application/json',
           (json:any, etag:string) => {
+            this.$scope.loadingVisibility2=false;
             this.$scope.single=json;
+            this.$scope.mapDetailVisiblity=true;
           },
           (response:ng.IHttpPromiseCallbackArg<any>) => {
+            this.$scope.loadingVisibility2=false;
             console.error(response);
           });
 
