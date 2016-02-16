0ad1ac27d5e9aea4ca824a005329c21ea6556b41
ifti24 <ifticse_kuet@hotmail.com>
2016-02-17 00:41:34 +0600

    Code Refactored.

--- ums-web/src/main/ts/controller/common/academic/ClassRoomInfo.ts
+++ ums-web/src/main/ts/controller/common/academic/ClassRoomInfo.ts
@@ -19,41 +19,31 @@ module ums {
 
       $.jgrid.defaults.responsive = true;
       $.jgrid.defaults.styleUI = 'Bootstrap';
+      $.extend($.jgrid.inlineEdit, { restoreAfterError: false });
 
-      var lastSel,
-          cancelEditing = function(myGrid) {
-            var lrid;
-            if (typeof lastSel !== "undefined") {
-              // cancel editing of the previous selected row if it was in editing state.
-              // jqGrid hold intern savedRow array inside of jqGrid object,
-              // so it is safe to call restoreRow method with any id parameter
-              // if jqGrid not in editing state
-              myGrid.jqGrid('restoreRow',lastSel);
-
-              // now we need to restore the icons in the formatter:"actions"
-              lrid = $.jgrid.jqID(lastSel);
-              $("tr#" + lrid + " div.ui-inline-edit, " + "tr#" + lrid + " div.ui-inline-del").show();
-              $("tr#" + lrid + " div.ui-inline-save, " + "tr#" + lrid + " div.ui-inline-cancel").hide();
-            }
-          };
-
+       var lastSel,
+      cancelEditing = function(myGrid) {
+        var lrid;
+        if (typeof lastSel !== "undefined") {
+          myGrid.jqGrid('restoreRow',lastSel);
+          lrid = $.jgrid.jqID(lastSel);
+          $("tr#" + lrid + " div.ui-inline-edit, " + "tr#" + lrid + " div.ui-inline-del").show();
+          $("tr#" + lrid + " div.ui-inline-save, " + "tr#" + lrid + " div.ui-inline-cancel").hide();
+        }
+      };
       $.extend($.jgrid.defaults, {
-        ajaxRowOptions: { contentType: "application/json", type: "PUT", async: true,
+        ajaxRowOptions: { contentType: "application/json", async: true,type:"PUT",
           beforeSend: function (jqXHR, settings) {
             jqXHR.setRequestHeader("Authorization", 'Basic ZHByZWdpc3RyYXI6MTIzNDU=');
             jqXHR.setRequestHeader("If-Match", '*');
           },
           complete: function(res, stat) {
-
             if (res.status==200 || res.status==204) {
               $("#jqGrid").trigger("reloadGrid");
-              //   return [true, 'OK' ];
-
             } else {
-              return false;
+              return [false, res.responseText ];
             }
           }
-
         },
         serializeRowData: function (data) {
           var propertyName, propertyValue, dataToSend = {};
@@ -70,18 +60,6 @@ module ums {
           return JSON.stringify(dataToSend);
         }
       });
-
-      /* THIS IS ONE FOR FORM EDIT AND DELETE */
-      /*
-      $.extend($.jgrid.del, {
-        ajaxEditOptions: {contentType: "application/json",async: true},
-        mtype: 'DELETE'
-      });
-      $.extend($.jgrid.edit, {
-        ajaxEditOptions: {contentType: "application/json",async: true},
-        mtype: 'PUT'
-      });
-     */
 
       var thisGrid =$("#jqGrid").jqGrid({
         datatype: "json",
@@ -115,7 +93,7 @@ module ums {
             label : 'Row',
             name: 'totalRow',
             width: 50,
-            editable: true // must set editable to true if you want to make the field editable
+            editable: true
           },
           {
             label: 'Column',
@@ -201,7 +179,6 @@ module ums {
                 }
                 lastSel = id;
                 $("#jqGrid").setGridParam({ editurl: "https://localhost/ums-webservice-common/academic/classroom/" + encodeURIComponent(id)});
-                //$("#jqGrid").jqGrid('editRow', id, true, null, null, "https://localhost/ums-webservice-common/academic/classroom/" + encodeURIComponent(id));
               }
 
             }
@@ -226,15 +203,7 @@ module ums {
           var lrid = $.jgrid.jqID(lastSel);
           if (!e) e = window.event; // get browser independent object
           var element = e.target || e.srcElement;
-          /*
-          alert("abc "+element);
-          var abc={
-            keys: true,
-            oneditfunc: function (rowId) {
-              $("input, select", element).focus(); // Now set focus on selected field
-            },
-            url:"https://localhost/ums-webservice-common/academic/classroom/"+ encodeURIComponent(id)
-          };*/
+
 
           $("#jqGrid").jqGrid('editRow',id,true,function() {
             var colModel = jQuery("#jqGrid").jqGrid ('getGridParam', 'colModel');
@@ -242,92 +211,33 @@ module ums {
             var input = $('#' + id + '_' + colName);
             console.log(input)
             setTimeout(function(){  input.get(0).focus(); }, 300);
-
           },null,"https://localhost/ums-webservice-common/academic/classroom/"+ encodeURIComponent(id));
-          //$("#jqGrid").jqGrid('editRow',id,abc);
           $("tr#" + lrid + " div.ui-inline-edit, " + "tr#" + lrid + " div.ui-inline-del").hide();
           $("tr#" + lrid + " div.ui-inline-save, " + "tr#" + lrid + " div.ui-inline-cancel").show();
         }
 
 
         });
-      function reload(rowid, result) {
-        $("#jqGrid").trigger("reloadGrid");
-      }
 
 
-/*
- $('#jqGrid').jqGrid('inlineNav','#jqGridPager', {
- addtext: "Add",
- edittext: "Edit",
- savetext: "Save",
- canceltext: "Cancel",
- addParams: {
- position: "afterSelected",
- addRowParams: myEditOptions
- },
- addedrow: "last",
- editParams: myEditOptions
- });
-
-
- jQuery("#jqGrid").jqGrid('inlineNav',"#jqGridPager",
- {
- editParams:{
- aftersavefunc: function() {
- var grid = $("#table");
- grid.trigger("reloadGrid");
- alert("aftersave fired for edit");
- }//aftersavefunc
- }//editParams
- }//options
- );//inlineNav definition
-      //$("#jqGrid″).jqGrid('delGridRow', selID, {url: '/QMSWebApp/DCRIndexDeleteServlet'} );
-      $("#jqGrid").jqGrid('inlineNav', '#jqGridPager',
-          {addParams: {
-            position: "last",
-             addRowParams: {
-              mtype: "POST",
-             url: "Service.svc/AddUser",
-              keys: true,
-               aftersavefunc: function(rowid, response) {alert("ifti");
-          }
+      var addOptions = {
+        keys: true,
+        type: "POST",
+        url: "AddUser",
+        successfunc: function () {
+          var $self = $(this);
+          setTimeout(function () {
+            $self.trigger("reloadGrid");
+          }, 50);
         }
-      },
-            editParams: { mtype: "PUT", keys: true, url: "Service.svc/EditUser" }
-          });
-      // add navigation bar with some built in actions for the grid
+      };
+      $("#jqGrid").jqGrid("inlineNav", "#jqGridPager", {
+        addParams: {
+          position: "last",
+          addRowParams: addOptions
+        }
+      });
 
-      $('#jqGrid').navGrid('#jqGridPager',
-          {
-            edit: false,
-            add: false,
-            del: false,
-            search: true,
-            refresh: true,
-            view: true,
-            position: "left",
-            cloneToTop: false
-          });
-
-
-      $("#jqGrid").jqGrid('navGrid', '#pager',
-          {edit: false, add: false, search: false}, {}, {},
-          { // Delete parameters
-            mtype: "DELETE",
-            serializeDelData: function () {
-              return ""; // don't send and body for the HTTP DELETE
-            },
-            onclickSubmit: function (params, postdata) {
-              params.url = '/api/widgets/' + encodeURIComponent(postdata);
-            }
-          });
-*/
-      function refreshGrid() {
-        alert("abc");
-        var grid = thisGrid;
-        grid.trigger("reloadGrid");
-      }
     }
 
   }
