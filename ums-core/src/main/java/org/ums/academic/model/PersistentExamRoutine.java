91e05a12f1f215d6faba51b375b579de100f3dc3
apu84 <apu.sust@gmail.com>
2016-03-02 23:52:08 +0600

    Merge conflicts

--- ums-core/src/main/java/org/ums/academic/model/PersistentExamRoutine.java
+++ ums-core/src/main/java/org/ums/academic/model/PersistentExamRoutine.java
@@ -6,19 +6,17 @@ import org.ums.domain.model.dto.ExamRoutineDto;
 import org.ums.domain.model.mutable.MutableExamRoutine;
 import org.ums.domain.model.readOnly.ExamRoutine;
 import org.ums.manager.ContentManager;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 
-public class PersistentExamRoutine  implements MutableExamRoutine {
+public class PersistentExamRoutine implements MutableExamRoutine {
 
-  private static ContentManager<ExamRoutine, MutableExamRoutine,Object> sExamRoutineManager;
+  private static ContentManager<ExamRoutine, MutableExamRoutine, Object> sExamRoutineManager;
 
   static {
     ApplicationContext applicationContext = AppContext.getApplicationContext();
-    sExamRoutineManager = (ContentManager<ExamRoutine, MutableExamRoutine, Object>)applicationContext.getBean("examRoutineManager");
+    sExamRoutineManager = (ContentManager<ExamRoutine, MutableExamRoutine, Object>) applicationContext.getBean("examRoutineManager");
   }
-
 
 
   private List<ExamRoutineDto> mExamRoutine;
@@ -31,13 +29,14 @@ public class PersistentExamRoutine  implements MutableExamRoutine {
 
   public PersistentExamRoutine() {
   }
+
   public PersistentExamRoutine(final MutableExamRoutine pOriginal) throws Exception {
     mExamRoutine = pOriginal.getRoutine();
   }
 
   @Override
   public void setRoutine(List<ExamRoutineDto> pRoutineList) {
-    mExamRoutine=pRoutineList;
+    mExamRoutine = pRoutineList;
   }
 
   @Override
@@ -53,8 +52,8 @@ public class PersistentExamRoutine  implements MutableExamRoutine {
 
   @Override
   public void delete() throws Exception {
-    if(mInsertType.equalsIgnoreCase("DateTimeProgram")){
-        //do delete
+    if(mInsertType.equalsIgnoreCase("DateTimeProgram")) {
+      //do delete
     }
 
   }
@@ -67,43 +66,53 @@ public class PersistentExamRoutine  implements MutableExamRoutine {
   public MutableExamRoutine edit() throws Exception {
     return null;
   }
+
   @Override
   public String getInsertType() {
     return mInsertType;
   }
+
   @Override
   public void setInsertType(String mInsertType) {
     this.mInsertType = mInsertType;
   }
+
   @Override
 
   public int getSemesterId() {
     return mSemesterId;
   }
+
   @Override
   public void setSemesterId(int mSemesterId) {
     this.mSemesterId = mSemesterId;
   }
+
   @Override
   public String getSemesterName() {
     return mSemesterName;
   }
+
   @Override
   public void setSemesterName(String mSemesterName) {
     this.mSemesterName = mSemesterName;
   }
+
   @Override
   public int getExamTypeId() {
     return mExamTypeId;
   }
+
   @Override
   public void setExamTypeId(int mExamTypeId) {
     this.mExamTypeId = mExamTypeId;
   }
+
   @Override
   public String getExamTypeName() {
     return mExamTypeName;
   }
+
   @Override
   public void setExamTypeName(String mExamTypeName) {
     this.mExamTypeName = mExamTypeName;
