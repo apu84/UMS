8042a03217d4c4198bbcdbb75909cb8c199424ae
apu84 <apu.sust@gmail.com>
2016-03-20 11:46:07 +0600

    Refactored shiro specific implementations to security package

--- ums-core/src/main/java/org/ums/configuration/UMSContext.java
+++ ums-core/src/main/java/org/ums/configuration/UMSContext.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.ums.authentication.UMSAuthenticationRealm;
 import org.ums.cache.*;
 import org.ums.manager.*;
 import org.ums.persistent.dao.*;
+import org.ums.security.authentication.UMSAuthenticationRealm;
 import org.ums.services.LoginService;
 import org.ums.util.Constants;
 
