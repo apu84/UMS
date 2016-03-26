3e4056b83f88674dd9dc6a3f339690a72d1bddb9
monjurmorshed793 <monjurmorshed793@gmail.com>
2016-03-26 17:52:54 +0600

    Code is littlebit butified.

---
+++ ums-api/src/main/java/org/ums/domain/model/mutable/MutableParameterSetting.java
@@ -0,0 +1,17 @@
+package org.ums.domain.model.mutable;
+
+import org.ums.domain.model.common.Mutable;
+import org.ums.domain.model.common.MutableIdentifier;
+import org.ums.domain.model.immutable.ParameterSetting;
+import org.ums.domain.model.immutable.Parameter;
+import org.ums.domain.model.immutable.Semester;
+
+/**
+ * Created by My Pc on 3/14/2016.
+ */
+public interface MutableParameterSetting extends ParameterSetting,Mutable,MutableLastModifier,MutableIdentifier<String> {
+  void setSemester(Semester pSemester);
+  void setParameter(Parameter pParameter) throws  Exception;
+  void setStartDate(String pStartDate);
+  void setEndDate(String pEndDate);
+}
---
+++ ums-api/src/main/java/org/ums/manager/ParameterSettingManager.java
@@ -0,0 +1,13 @@
+package org.ums.manager;
+
+import org.ums.domain.model.immutable.ParameterSetting;
+import org.ums.domain.model.mutable.MutableParameterSetting;
+
+import java.util.List;
+
+/**
+ * Created by My Pc on 3/14/2016.
+ */
+public interface ParameterSettingManager extends ContentManager<ParameterSetting,MutableParameterSetting,String> {
+  public List<ParameterSetting> getBySemester(int semesterId);
+}
