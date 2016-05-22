fbedf608bf7b37fc237e58f5cdd72524ab7581e1
monjurmorshed793 <monjurmorshed793@gmail.com>
2016-05-22 17:06:47 +0600

    Cache is disabled for seatplan due to some issues.

--- ums-core/src/main/java/org/ums/cache/SeatPlanCache.java
+++ ums-core/src/main/java/org/ums/cache/SeatPlanCache.java
@@ -1,5 +1,6 @@
 package org.ums.cache;
 
+import org.ums.domain.model.common.LastModifier;
 import org.ums.domain.model.immutable.SeatPlan;
 import org.ums.domain.model.mutable.MutableSeatPlan;
 import org.ums.manager.CacheManager;
@@ -11,13 +12,14 @@ import java.util.List;
 /**
  * Created by My Pc on 5/8/2016.
  */
-public class SeatPlanCache extends ContentCache<SeatPlan,MutableSeatPlan,Integer,SeatPlanManager> implements SeatPlanManager {
+public class SeatPlanCache extends ContentCache<SeatPlan, MutableSeatPlan, Integer, SeatPlanManager> implements SeatPlanManager {
 
   private CacheManager mCacheManager;
 
-  public SeatPlanCache(final CacheManager pCacheManager){
+  public SeatPlanCache(final CacheManager pCacheManager) {
     mCacheManager = pCacheManager;
   }
+
   @Override
   protected CacheManager getCacheManager() {
     return mCacheManager;
@@ -25,22 +27,22 @@ public class SeatPlanCache extends ContentCache<SeatPlan,MutableSeatPlan,Integer
 
   @Override
   protected String getCacheKey(Integer pId) {
-    return CacheUtil.getCacheKey(SeatPlan.class,pId);
+    return CacheUtil.getCacheKey(SeatPlan.class, pId);
   }
 
   @Override
-  public List<SeatPlan> getBySemesterAndGroupAndExamType(int pSemesterId, int pGropNo,int pExamType) {
-    return getManager().getBySemesterAndGroupAndExamType(pSemesterId,pGropNo,pExamType);
+  public List<SeatPlan> getBySemesterAndGroupAndExamType(int pSemesterId, int pGropNo, int pExamType) {
+    return getManager().getBySemesterAndGroupAndExamType(pSemesterId, pGropNo, pExamType);
   }
 
   @Override
-  public List<SeatPlan> getByRoomSemesterGroupExamType(int pRoomId, int pSemesterId, int pGroupNo,int pExamType) {
-    return getManager().getByRoomSemesterGroupExamType(pRoomId,pSemesterId,pGroupNo,pExamType);
+  public List<SeatPlan> getByRoomSemesterGroupExamType(int pRoomId, int pSemesterId, int pGroupNo, int pExamType) {
+    return getManager().getByRoomSemesterGroupExamType(pRoomId, pSemesterId, pGroupNo, pExamType);
   }
 
   @Override
   public int deleteBySemesterGroupExamType(int pSemesterId, int pGroupNo, int pExamType) {
-    return getManager().deleteBySemesterGroupExamType(pSemesterId,pGroupNo,pExamType);
+    return getManager().deleteBySemesterGroupExamType(pSemesterId, pGroupNo, pExamType);
   }
 
   @Override
@@ -50,24 +52,43 @@ public class SeatPlanCache extends ContentCache<SeatPlan,MutableSeatPlan,Integer
 
   @Override
   public int checkIfExistsBySemesterGroupTypeRoomRowAndCol(int pSemesterId, int pGroupNo, int pType, int pRoomId, int pRow, int pCol) {
-    return getManager().checkIfExistsBySemesterGroupTypeRoomRowAndCol(pSemesterId,pGroupNo,pType,pRoomId,pRow,pCol);
+    return getManager().checkIfExistsBySemesterGroupTypeRoomRowAndCol(pSemesterId, pGroupNo, pType, pRoomId, pRow, pCol);
   }
 
   @Override
-  public int checkIfExistsByRoomSemesterGroupExamType(int pRoomId, int pSemesterId, int pGroupNo, int pExamType) throws Exception{
-    int pCacheKey = generateCacheKeyForRoomSemesterGroupExamType(pRoomId, pSemesterId, pGroupNo, pExamType);
-    String cacheKey = getCacheKey(pCacheKey);
+  public int checkIfExistsByRoomSemesterGroupExamType(int pRoomId, int pSemesterId, int pGroupNo, int pExamType) throws Exception {
+    /*long pCacheKey = generateCacheKeyForRoomSemesterGroupExamType(pRoomId, pSemesterId, pGroupNo, pExamType);
+    String cacheKey = CacheUtil.getCacheKey(SeatPlan.class, pCacheKey);
     Object pReadonly = getCacheManager().get(cacheKey);
     if (pReadonly == null) {
-      pReadonly = getManager().checkIfExistsByRoomSemesterGroupExamType(pRoomId, pSemesterId, pGroupNo, pExamType);;
-      getCacheManager().put(cacheKey, pReadonly);
+      pReadonly = getManager().checkIfExistsByRoomSemesterGroupExamType(pRoomId, pSemesterId, pGroupNo, pExamType);
+      TemporaryContainer temporaryContainer = new TemporaryContainer(pReadonly);
+      getCacheManager().put(cacheKey, temporaryContainer);
     }
-    return (int)pReadonly;
+    return (Integer) ((TemporaryContainer)pReadonly).getContain();*/
+    return getManager().checkIfExistsByRoomSemesterGroupExamType(pRoomId,pSemesterId,pGroupNo,pExamType);
   }
 
-  protected int generateCacheKeyForRoomSemesterGroupExamType(int pRoomId, int pSemesterId, int pGroupNo, int pExamType) {
+  protected long generateCacheKeyForRoomSemesterGroupExamType(int pRoomId, int pSemesterId, int pGroupNo, int pExamType) {
     StringBuilder builder = new StringBuilder();
     builder.append(pRoomId).append(pSemesterId).append(pGroupNo).append(pExamType);
-    return Integer.parseInt(builder.toString());
+    return Long.parseLong(builder.toString());
+  }
+
+  class TemporaryContainer implements LastModifier {
+    private Object contain;
+
+    public TemporaryContainer(Object pContain) {
+      contain = pContain;
+    }
+
+    public Object getContain() {
+      return contain;
+    }
+
+    @Override
+    public String getLastModified() {
+      return "";
+    }
   }
 }
