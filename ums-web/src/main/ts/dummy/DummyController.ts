f58d905cb38dbc5301f03bc3867949580c4ad3eb
apu84 <apu.sust@gmail.com>
2016-06-12 18:05:07 +0600

    Updated the amChart example using how to update data

--- ums-web/src/main/ts/dummy/DummyController.ts
+++ ums-web/src/main/ts/dummy/DummyController.ts
@@ -51,6 +51,32 @@ module ums {
           fillAlphas: 1
         }]
       };
+
+
+      $timeout(()=> {
+        var updatedData = [{
+          year: 2010,
+          income: 23.5,
+          expenses: 18.1
+        }, {
+          year: 2011,
+          income: 26.2,
+          expenses: 22.8
+        }, {
+          year: 2012,
+          income: 30.1,
+          expenses: 23.9
+        }, {
+          year: 2013,
+          income: 29.5,
+          expenses: 25.1
+        }, {
+          year: 2014,
+          income: 24.6,
+          expenses: 25
+        }];
+        $scope.$broadcast("amCharts.updateData", updatedData);
+      }, 5000);
     }
 
   }
