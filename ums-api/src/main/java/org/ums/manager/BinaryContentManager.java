fb01512ba93943966f8a3725f8d765053f89938c
apu84 <apu.sust@gmail.com>
2016-07-10 22:44:18 +0600

    Added initial backend for file browser

--- ums-api/src/main/java/org/ums/manager/BinaryContentManager.java
+++ ums-api/src/main/java/org/ums/manager/BinaryContentManager.java
@@ -2,6 +2,7 @@ package org.ums.manager;
 
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public interface BinaryContentManager<T> {
@@ -15,7 +16,9 @@ public interface BinaryContentManager<T> {
   String create(T pData, String pIdentifier, Domain pDomain) throws Exception;
 
   enum Domain {
-    PICTURE(1);
+    PICTURE(1),
+    COURSE_MATERIAL(2)
+    ;
 
     private static final Map<Integer, Domain> lookup
         = new HashMap<>();
@@ -39,4 +42,29 @@ public interface BinaryContentManager<T> {
       return this.typeCode;
     }
   }
+
+
+  List<Map<String, String>> list(String pPath, Domain pDomain);
+
+  Map<String, String> rename(String pOldPath, String pNewPath, Domain pDomain);
+
+  Map<String, String> move(List<String> pItems, String pNewPath, Domain pDomain);
+
+  Map<String, String> copy(List<String> pItems, String pNewPath, Domain pDomain);
+
+  Map<String, String> remove(List<String> pItems, Domain pDomain);
+
+  Map<String, byte[]> content(String pPath, Domain pDomain);
+
+  Map<String, String> createFolder(String pNewPath, Domain pDomain);
+
+  Map<String, String> compress(List<String> pItems, String pNewPath, String pNewFileName, Domain pDomain);
+
+  Map<String, String> extract(String pZippedItem, String pDestination, Domain pDomain);
+
+  Map<String, String> upload(byte[] pFileContent, String pPath, Domain pDomain);
+
+  byte[] download(String pPath, Domain pDomain);
+
+  byte[] downloadAsZip(List<String> pItems, String pNewFileName, Domain pDomain);
 }
