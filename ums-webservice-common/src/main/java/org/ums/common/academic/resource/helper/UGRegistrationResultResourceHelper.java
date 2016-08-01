eb09bd1a8dd34f123e7e4b8ba7e93e93b9ec0862
monjurmorshed793 <monjurmorshed793@gmail.com>
2016-08-01 13:31:43 +0600

    Following features added *Sortable items are more responsive. Now, sortable items can be splitted recursively as one wish. *Split number error is checked if any one tries to give abnormal split values. *Merge operations are more acurate and responsive. *Angular UI sortable library is used. *Jquery usases are transfered into directive so that, they can be more responsive and work better with angualr directives. *Now, a command window will pop up  when the user tries to recreate, to ensure data change. *Sub reporting is done. Previously, only complete sub group could be generated. But, this time, if a user wants to see partial seat plan, he can do that. *Seat plan algorithm issue is solved, where, there were more than current students inthe seat plan. *Duplicate issue is solved in the seat plan algorithm. Now, for both CCI and Regular exam, there will be a definite mappings of the student for the respective situation. *In the seat plan report, whether a student is giving carry or clearance or improvement, is identified as per rules.

---
+++ ums-webservice-common/src/main/java/org/ums/common/academic/resource/helper/UGRegistrationResultResourceHelper.java
@@ -0,0 +1,98 @@
+package org.ums.common.academic.resource.helper;
+
+import org.apache.shiro.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.ums.cache.LocalCache;
+import org.ums.common.ResourceHelper;
+import org.ums.common.builder.Builder;
+import org.ums.common.builder.UGRegistrationResultBuilder;
+import org.ums.domain.model.immutable.Student;
+import org.ums.domain.model.immutable.UGRegistrationResult;
+import org.ums.domain.model.mutable.MutableUGRegistrationResult;
+import org.ums.manager.ContentManager;
+import org.ums.manager.StudentManager;
+import org.ums.manager.UGRegistrationResultManager;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+/**
+ * Created by My Pc on 7/13/2016.
+ */
+
+@Component
+public class UGRegistrationResultResourceHelper extends ResourceHelper<UGRegistrationResult,MutableUGRegistrationResult,Integer>{
+
+  @Autowired
+  UGRegistrationResultManager mManager;
+
+  @Autowired
+  StudentManager mStudentManager;
+
+  @Autowired
+  UGRegistrationResultBuilder mBuilder;
+
+  @Override
+  public Response post(JsonObject pJsonObject, UriInfo pUriInfo) throws Exception {
+    return null;
+  }
+
+  public JsonObject getResultForCarryClearanceAndImprovement(final Request pRequest,final UriInfo pUriInfo) throws Exception{
+
+    String mStudentId = SecurityUtils.getSubject().getPrincipal().toString();
+    Student student = mStudentManager.get(mStudentId);
+    List<UGRegistrationResult> results = mManager.getCarryClearanceImprovementCoursesByStudent(student.getSemesterId(),student.getId());
+    JsonObjectBuilder object = Json.createObjectBuilder();
+    JsonArrayBuilder children = Json.createArrayBuilder();
+    LocalCache localCache = new LocalCache();
+
+    for(UGRegistrationResult result:results){
+      children.add(toJson(result, pUriInfo, localCache));
+
+    }
+
+    object.add("entries",children);
+    localCache.invalidate();
+
+    return object.build();
+  }
+
+  public JsonObject getResultForApplicationCCIOfCarryClearanceAndImprovement(List<UGRegistrationResult> results,final UriInfo pUriInfo)throws Exception{
+    JsonObjectBuilder object = Json.createObjectBuilder();
+    JsonArrayBuilder children = Json.createArrayBuilder();
+    LocalCache localCache = new LocalCache();
+
+    for(UGRegistrationResult result:results){
+      children.add(toJson(result, pUriInfo, localCache));
+
+    }
+
+    object.add("entries",children);
+    localCache.invalidate();
+
+    return object.build();
+  }
+
+
+  @Override
+  protected UGRegistrationResultManager getContentManager() {
+    return mManager;
+  }
+
+  @Override
+  protected Builder<UGRegistrationResult, MutableUGRegistrationResult> getBuilder() {
+    return mBuilder;
+  }
+
+  @Override
+  protected String getEtag(UGRegistrationResult pReadonly) {
+    return "1";
+  }
+}
