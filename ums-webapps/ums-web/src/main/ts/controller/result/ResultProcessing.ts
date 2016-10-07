bdc2a9e3b3643d900b9cb7c8c73e24da95b178c0
ifti24 <ifticse_kuet@hotmail.com>
2016-10-07 18:58:56 +0600

    Result Processing UI.

--- ums-webapps/ums-web/src/main/ts/controller/result/ResultProcessing.ts
+++ ums-webapps/ums-web/src/main/ts/controller/result/ResultProcessing.ts
@@ -10,6 +10,8 @@ module ums {
 
 
       this.$scope.fetchStatusInfo = this.fetchStatusInfo.bind(this);
+      this.$scope.showYearSemesterWise=this.showYearSemesterWise.bind(this);
+      this.$scope.showDefault=this.showDefault.bind(this);
     }
 
     private fetchStatusInfo(): void {
@@ -21,6 +23,34 @@ module ums {
 
     }
 
+    private showYearSemesterWise(deptId:String):void{
+      $("#"+deptId+"_view1").hide('slide', { direction: 'right', easing: 'easeOutBounce' },400);
+
+      if($("#"+deptId+"_download")){
+        $("#"+deptId+"_download").hide('slide', { direction: 'right', easing: 'easeOutBounce' },200);
+      }
+
+      if($("#"+deptId+"_publish")){
+        $("#"+deptId+"_publish").hide('slide', { direction: 'right', easing: 'easeOutBounce' },200);
+      }
+      setTimeout(function(){
+        $("#"+deptId+"_view2").fadeIn(200);
+      }, 400);
+
+    }
+private showDefault(deptId:String):void{
+  $("#"+deptId+"_view2").hide('slide', { direction: 'right', easing: 'easeOutBounce' },200);
+
+  setTimeout(function(){
+    $("#"+deptId+"_view1").fadeIn(200);
+    if($("#"+deptId+"_download")){
+      $("#"+deptId+"_download").fadeIn(100);
+    if($("#"+deptId+"_publish")){
+      $("#"+deptId+"_publish").fadeIn(100);
+    }
+  }, 200);
+
+}
 
   }
 
