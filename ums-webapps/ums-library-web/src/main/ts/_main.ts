6b92d6f523c05ae862f2120eda4be7fe637f3f0e
Md. Moshlehuddin Mazumder <apu@escenic.com>
2016-11-28 11:08:08 +0600

    Added ums-library module

---
+++ ums-webapps/ums-library-web/src/main/ts/_main.ts
@@ -0,0 +1,78 @@
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/angular.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/angular-route.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jquery.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jquery.validate.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jqueryui.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jquery.slimScroll.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jquery.notific8.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jquery.notify.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/select2.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/bootstrap.selectpicker.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/zabuto_calendar.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/bootstrap-switch.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/uri.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/angular-cookies.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jquery.jqGrid.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/angular-ui-sortable.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/moment.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jquery.dragNdrop.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/jquery.tab.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/bootstrap.modal.d.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/lib/handsontable.d.ts'/>
+
+/// <reference path='ums-library.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/service/BaseUri.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/service/Notify.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/service/HttpClient.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/service/FileUpload.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/service/CookieService.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/service/Settings.ts'/>
+
+/// <reference path='../../../../ums-web-core/src/main/ts/controller/AppController.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/controller/MainController.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/controller/ChangePassword.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/controller/Logout.ts'/>
+
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/FlushCache.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/LeftMenu.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/DisableOperation.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/Validate.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/ProgramSelector.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/FileModel.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/Image.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/UITab.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/MainNavigation.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/BindHtml.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/DatePicker.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/AutoComplete.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/Spinner.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/ProfilePicture.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/Grid.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/SelectPicker.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/ConvertToNumber.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/Modal.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/Notification.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/directive/ConfirmationDIalog.ts'/>
+
+/// <reference path='../../../../ums-web-core/src/main/ts/interceptor/Unauthorized.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/interceptor/ValidationExceptions.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/interceptor/ServerExceptions.ts'/>
+
+/// <reference path='../../../../ums-web-core/src/main/ts/filter/TrustAsHtml.ts'/>
+
+/// <reference path='../../../../ums-web-core/src/main/ts/model/ChangePasswordModel.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/model/User.ts'/>
+
+/// <reference path='../../../../ums-web-core/src/main/ts/grid/GridConfig.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/grid/GridDecorator.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/grid/GridOptions.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/grid/InlineNavigationOptions.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/grid/InlineNavigationOptionsImpl.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/grid/JqGridApi.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/grid/JqGridApiImpl.ts'/>
+
+/// <reference path='../../../../ums-web-core/src/main/ts/util/UmsUtil.ts'/>
+/// <reference path='../../../../ums-web-core/src/main/ts/util/UriUtil.ts'/>
+
+/// <reference path='data.ts'/>
+
