1abfe88bcf7d682906a022fc98c74a487eaa7f86
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-01-07 09:46:43 +0600

    Admission department selection feature added.

--- ums-webapps/ums-web/src/main/ts/controller/admission/AdmissionDepartmentSelection.ts
+++ ums-webapps/ums-web/src/main/ts/controller/admission/AdmissionDepartmentSelection.ts
@@ -4,12 +4,85 @@
 
 module ums{
   interface IAdmissionDepartmentSelection extends ng.IScope{
+    semesters:Array<Semester>;
+    semester:Semester;
+    programTypes:Array<IProgramType>;
+    programType:IProgramType;
+    faculty:Faculty;
+    faculties:Array<Faculty>;
+    meritTypes:Array<IMeritListType>;
+    meritType:IMeritListType;
 
+    getSemesters:Function;
+  }
+
+  interface  IProgramType{
+    id:string;
+    name:string;
+  }
+
+  interface IMeritListType{
+    id:string;
+    name:string;
   }
 
   class AdmissionDepartmentSelection{
 
+    public static $inject = ['appConstants','HttpClient','$scope','$q','notify','$sce','$window','semesterService','facultyService', 'admissionStudentService'];
+    constructor(private appConstants: any,
+                private httpClient: HttpClient,
+                private $scope: IAdmissionDepartmentSelection,
+                private $q:ng.IQService,
+                private notify: Notify,
+                private $sce:ng.ISCEService,
+                private $window:ng.IWindowService,
+                private semesterService: SemesterService,
+                private facultyService: FacultyService,
+                private admissionStudentService:AdmissionStudentService) {
+
+      $scope.programTypes=appConstants.programType;
+      $scope.programType = $scope.programTypes[0];
+
+      $scope.getSemesters= this.getSemesters.bind(this);
+
+      this.getFaculties();
+      this.getSemesters();
+      this.getMeritListTypes();
+
+
+    }
+
+    private getMeritListTypes():void{
+      this.$scope.meritTypes = [];
+      this.$scope.meritTypes = this.appConstants.meritListTypes;
+      this.$scope.meritType = this.$scope.meritTypes[1];
+    }
+
+    private getSemesters():void{
+      this.semesterService.fetchSemesters(+this.$scope.programType.id,5).then((semesters:Array<Semester>)=>{
+        this.$scope.semesters=semesters;
+        for(var i=0;i<semesters.length;i++){
+          if(semesters[i].status==2){
+            this.$scope.semester = semesters[i];
+            break;
+          }
+        }
+      });
+    }
+
+    private getFaculties(){
+      this.facultyService.getAllFaculties().then((faculties:Array<Faculty>)=>{
+        this.$scope.faculties=[];
+        for(var i=0;i<faculties.length;i++){
+            this.$scope.faculties.push(faculties[i]);
+        }
+        this.$scope.faculty=faculties[0];
+
+      });
+    }
+
   }
+
 
   UMS.controller("AdmissionDepartmentSelection", AdmissionDepartmentSelection);
 }
