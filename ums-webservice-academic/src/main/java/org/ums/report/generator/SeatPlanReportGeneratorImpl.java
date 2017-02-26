d2c909b9245d9fe54e26439aa3e753b337a99cf8
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-02-26 15:19:46 +0600

    Student sticker bug fixed.

--- ums-webservice-academic/src/main/java/org/ums/report/generator/SeatPlanReportGeneratorImpl.java
+++ ums-webservice-academic/src/main/java/org/ums/report/generator/SeatPlanReportGeneratorImpl.java
@@ -1920,8 +1920,12 @@ public class SeatPlanReportGeneratorImpl implements SeatPlanReportGenerator {
       rightCell.setPaddingLeft(13.0f);
       boolean dataEnd = false;
       for(int i = 1; i <= 2; i++) {
-        if(seatPlans.size() != 0) {
+
+        if(seatPlans.size() > 0) {
           SeatPlanReportDto seatPlanReportDto = seatPlans.get(0);
+          if(seatPlanReportDto.getStudentId().equals("140107092")) {
+            boolean foundI = true;
+          }
           Paragraph paragraph = new Paragraph();
           String roomInfo = seatPlanReportDto.getRoomNo();
           paragraph.add(roomInfo);
@@ -2023,7 +2027,7 @@ public class SeatPlanReportGeneratorImpl implements SeatPlanReportGenerator {
 
       rowCounter += 1;
 
-      if(rowCounter == totalRow || dataEnd == true) {
+      if(rowCounter >= totalRow || seatPlans.size() == 0) {
         document.add(masterTable);
         rowCounter = 0;
         masterTable = new PdfPTable(2);
@@ -2193,6 +2197,153 @@ public class SeatPlanReportGeneratorImpl implements SeatPlanReportGenerator {
 
   }
 
+  @Override
+  public void createRoomWiseQuestionDistributionReport(int pSemesterId, ExamType pExamType, OutputStream pOutputStream) throws IOException, DocumentException {
+
+    List<SeatPlan> seatPlans = mSeatPlanManager.getSittingArrangement(pSemesterId, pExamType);
+
+    Map<String, List<SeatPlan>> seatPlanMapUnordered = seatPlans
+        .stream()
+        .sorted(Comparator.comparing(s->s.getStudent().getProgram().getId()))
+        .collect(Collectors.groupingBy(s-> s.getStudent().getProgram().getId().toString()+s.getStudent().getCurrentYear()+s.getStudent().getCurrentAcademicSemester()+""));
+
+    Map<String, List<SeatPlan>> seatPlanMap = new TreeMap<>();
+    seatPlanMap = seatPlanMapUnordered;
+    List sortedSeatPlanMapKeys = new ArrayList(seatPlanMap.keySet());
+    Collections.sort(sortedSeatPlanMapKeys);
+
+    /*seatPlanMapUnordered.entrySet()
+        .stream()
+        .sorted(Map.Entry.<Integer, List<SeatPlan>> comparingByKey())
+        .forEachOrdered(x-> seatPlanMap.put(x.getKey(), x.getValue()));*/
+
+    Document document = new Document();
+    document.addTitle("Seat Plan Attendence Sheet");
+    Semester semester = mSemesterManager.get(pSemesterId);
+
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PdfWriter writer = PdfWriter.getInstance(document, baos);
+    document.open();
+//    SittingArrangement sittingArrangement = new SittingArrangement();
+//    writer.setPageEvent(sittingArrangement);
+    document.setPageSize(PageSize.A4);
+
+    Font lightFont = FontFactory.getFont(FontFactory.TIMES, 12);
+
+    Font boldFont = FontFactory.getFont(FontFactory.TIMES_BOLD, 12);
+
+
+    document.newPage();
+
+    for(int i=0;i<sortedSeatPlanMapKeys.size();i++) {
+      //PdfContentByte cb = writer.getDirectContent();
+      List<SeatPlan> seatPlansOfTheMap = seatPlanMap.get(sortedSeatPlanMapKeys.get(i));
+      /*sittingArrangement.setSittingArrangement(pExamType, semester, seatPlansOfTheMap.get(0).getStudent());*/
+      /*sittingArrangement.setSittingArrangement(pExamType, semester, seatPlansOfTheMap.get(0).getStudent());
+      //writer.setPageEvent(sittingArrangement);
+      //document.open();
+      Paragraph paragraph = new Paragraph("Hello there");
+      document.add(paragraph);
+      *//*Map<String,List<SeatPlan>> seatPlanMapByRoomNo = seatPlansOfTheMap.stream().collect(Collectors.groupingBy(s-> s.getClassRoom().getRoomNo()));
+
+
+      for(String roomKey: seatPlanMapByRoomNo.keySet()){
+
+      }*/
+
+
+      Map<Long,List<SeatPlan>> seatPlanMapByRoomNoUnordered = seatPlansOfTheMap
+          .stream()
+          .collect(Collectors.groupingBy(s-> s.getClassRoom().getId()));
+
+      Map<Long, List<SeatPlan>> seatPlanMapByRoomNo= new TreeMap<>();
+      seatPlanMapByRoomNo = seatPlanMapByRoomNoUnordered;
+      List seatPlanMapByRoomNoKeys = new ArrayList(seatPlanMapByRoomNo.keySet());
+      PdfPTable headerTable = getSittingArrangementHeader(pExamType, boldFont, seatPlansOfTheMap, seatPlanMapByRoomNo);
+      document.add(headerTable);
+      Collections.sort(seatPlanMapByRoomNoKeys);
+
+
+      /*seatPlanMapByRoomNoUnordered.entrySet()
+          .stream()
+          .sorted(Map.Entry.<Long, List<SeatPlan>>comparingByKey())
+          .forEachOrdered(x-> seatPlanMapByRoomNo.put(x.getKey(), x.getValue()));*/
+
+      float[] tableWidth = new float[] {2, 4,4,4, 2};
+      Font lightFontInner= new Font();
+      lightFontInner = FontFactory.getFont(FontFactory.TIMES, 12);
+
+      Font boldFontInner = new Font();
+      boldFontInner = FontFactory.getFont(FontFactory.TIMES_BOLD, 12);
+
+      Integer seatPlanKeySize = new Integer(seatPlanMapByRoomNo.size());
+      if(seatPlanKeySize<=6){
+        lightFontInner = FontFactory.getFont(FontFactory.TIMES, 18);
+
+        boldFontInner = FontFactory.getFont(FontFactory.TIMES_BOLD, 18);
+      }
+      else if(seatPlanKeySize>6 && seatPlanKeySize<=10){
+        lightFontInner = FontFactory.getFont(FontFactory.TIMES, 14);
+
+        boldFontInner = FontFactory.getFont(FontFactory.TIMES_BOLD, 14);
+      }else if(seatPlanKeySize>10 && seatPlanKeySize<=16){
+        lightFontInner = FontFactory.getFont(FontFactory.TIMES, 12);
+
+        boldFontInner = FontFactory.getFont(FontFactory.TIMES_BOLD, 12);
+      }else{
+        lightFontInner = FontFactory.getFont(FontFactory.TIMES, 10);
+
+        boldFontInner = FontFactory.getFont(FontFactory.TIMES_BOLD, 10);
+      }
+
+      PdfPTable sittingArrangementTable = new PdfPTable(6);
+      sittingArrangementTable.setWidths(tableWidth);
+      sittingArrangementTable.setWidthPercentage(100);
+      sittingArrangementTable.setPaddingTop(20);
+      PdfPCell tableHeaderCell = new PdfPCell();
+      Paragraph cellData = new Paragraph("Room No.", boldFontInner);
+      addHeaderToSittingArrangementDataTable(sittingArrangementTable, tableHeaderCell, cellData);
+
+      cellData = new Paragraph("Student's Id", boldFontInner);
+      addHeaderToSittingArrangementDataTable(sittingArrangementTable, tableHeaderCell, cellData);
+
+      cellData = new Paragraph("No. of Students", boldFontInner);
+      addHeaderToSittingArrangementDataTable(sittingArrangementTable, tableHeaderCell, cellData);
+
+      for(int j=0;j<seatPlanMapByRoomNoKeys.size();j++){
+        PdfPCell bodyCell = new PdfPCell();
+        Paragraph bodyParagraph = new Paragraph(seatPlanMapByRoomNo.get(seatPlanMapByRoomNoKeys.get(j)).get(0).getClassRoom().getRoomNo(), lightFontInner);
+        bodyCell.addElement(bodyParagraph);
+        bodyCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        bodyCell.setVerticalAlignment(Element.ALIGN_CENTER);
+        sittingArrangementTable.addCell(bodyCell);
+        String[] studentIds = new String[seatPlanMapByRoomNo.get(seatPlanMapByRoomNoKeys.get(j)).size()];
+        List<SeatPlan> bodySeatPlanList = seatPlanMapByRoomNo.get(seatPlanMapByRoomNoKeys.get(j));
+        for(int x=0;x<bodySeatPlanList.size();x++){
+          studentIds[x]=bodySeatPlanList.get(x).getStudent().getId();
+        }
+        bodyCell= new PdfPCell (new Paragraph(String.join(", ",studentIds), lightFontInner));
+        bodyCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+        bodyCell.setVerticalAlignment(Element.ALIGN_CENTER);
+        sittingArrangementTable.addCell(bodyCell);
+
+        bodyCell= new PdfPCell (new Paragraph(bodySeatPlanList.size()+"", lightFontInner));
+        bodyCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        sittingArrangementTable.addCell(bodyCell);
+      }
+
+      document.add(sittingArrangementTable);
+
+      document.newPage();
+
+
+    }
+
+    document.close();
+    baos.writeTo(pOutputStream);
+  }
+
   private void addHeaderToSittingArrangementDataTable(PdfPTable pSittingArrangementTable,
       PdfPCell pTableHeaderCell, Paragraph pCellData) {
     pCellData.setAlignment(Element.ALIGN_CENTER);
@@ -2249,7 +2400,8 @@ public class SeatPlanReportGeneratorImpl implements SeatPlanReportGenerator {
     headerParagraph.setPaddingTop(-5f);
     headerCell.addElement(headerParagraph);
 
-    headerParagraph = new Paragraph("Total number allocated rooms: " + seatPlanMapByRoomNo.size(), pBoldFont);
+    headerParagraph =
+        new Paragraph("Total number allocated rooms: " + seatPlanMapByRoomNo.size(), pBoldFont);
     headerParagraph.setAlignment(Element.ALIGN_CENTER);
     headerParagraph.setPaddingTop(-5f);
     headerParagraph.setSpacingBefore(-5f);
