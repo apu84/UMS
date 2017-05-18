e496da7890319dd82a273582b894b149aa8c404d
Md. Moshlehuddin Mazumder <apu@escenic.com>
2017-05-18 17:37:47 +0600

    -Continued work on Latefee and InstallmentSettings

---
+++ ums-webservice-academic/src/main/java/org/ums/academic/resource/fee/installment/InstallmentSettingsBuilder.java
@@ -0,0 +1,35 @@
+package org.ums.academic.resource.fee.installment;
+
+import org.apache.commons.lang.Validate;
+import org.springframework.stereotype.Component;
+import org.ums.builder.Builder;
+import org.ums.cache.LocalCache;
+import org.ums.domain.model.immutable.ProgramType;
+import org.ums.domain.model.immutable.Semester;
+import org.ums.fee.semesterfee.InstallmentSettings;
+import org.ums.fee.semesterfee.MutableInstallmentSettings;
+
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.core.UriInfo;
+
+@Component
+public class InstallmentSettingsBuilder implements Builder<InstallmentSettings, MutableInstallmentSettings> {
+  @Override
+  public void build(JsonObjectBuilder pBuilder, InstallmentSettings pReadOnly, UriInfo pUriInfo, LocalCache pLocalCache) {
+    Semester semester = (Semester) pLocalCache.cache(pReadOnly::getSemester, pReadOnly.getSemesterId(), Semester.class);
+    pBuilder.add("semesterName", semester.getName());
+    pBuilder.add("semesterId", semester.getId());
+    pBuilder.add("enabled", pReadOnly.isEnabled());
+    pBuilder.add("lastModified", pReadOnly.getLastModified());
+  }
+
+  @Override
+  public void build(MutableInstallmentSettings pMutable, JsonObject pJsonObject, LocalCache pLocalCache) {
+    Validate.notEmpty(pJsonObject);
+    Validate.notEmpty(pJsonObject.getString("semesterId"));
+    Validate.notEmpty(pJsonObject.getString("enabled"));
+    pMutable.setSemesterId(Integer.parseInt(pJsonObject.getString("semesterId")));
+    pMutable.setEnabled(Boolean.parseBoolean(pJsonObject.getString("enabled")));
+  }
+}
