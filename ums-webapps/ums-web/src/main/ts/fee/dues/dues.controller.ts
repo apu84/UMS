86bdde22825e1448cdc1b892378c366083de6a9f
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-06-20 10:22:31 +0600

    Temporary bug fixed in dues.controller.ts and student.dues.service.ts

--- ums-webapps/ums-web/src/main/ts/fee/dues/dues.controller.ts
+++ ums-webapps/ums-web/src/main/ts/fee/dues/dues.controller.ts
@@ -17,12 +17,12 @@ module ums {
     }
 
     public addFilter(): void {
-      filter.push({key: this.selectedFilter, value: this.selectedFilterValue});
+      this.filter.push({key: this.selectedFilter, value: this.selectedFilterValue});
     }
 
     public removeFilter(removedFilter): void {
-      for (let i = 0; i < filter.length; i++) {
-        if (filter[i].key === removedFilter) {
+      for (let i = 0; i < this.filter.length; i++) {
+        if (this.filter[i].key === removedFilter) {
           this.filter.splice(i, 1);
         }
       }
