34a7fa6345d931586bd392cb0025b7d1a9439ff6
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-07-09 15:57:47 +0600

    Initial ftp-server integration done with spring-integration framework.

--- ums-webservice-core/src/main/java/org/ums/resource/ProfilePicture.java
+++ ums-webservice-core/src/main/java/org/ums/resource/ProfilePicture.java
@@ -1,7 +1,15 @@
 package org.ums.resource;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.ums.integration.FileWriterGateway;
+import org.ums.integration.MessagePrinter;
+import org.ums.manager.BinaryContentManager;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,22 +17,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-import org.ums.manager.BinaryContentManager;
+import java.io.InputStream;
 
 @Component
 @Path("/profilePicture")
 @Produces("image/png")
 public class ProfilePicture extends Resource {
 
+  private static final Logger mLogger = LoggerFactory.getLogger(ProfilePicture.class);
+
   @Autowired
   @Qualifier("fileContentManager")
   BinaryContentManager<byte[]> mBinaryContentManager;
+
+  @Autowired
+  private FileWriterGateway mGateway;
+
+  @Autowired
+  MessagePrinter mMessagePrinter;
 
   @GET
   public Response get(final @Context Request pRequest) {
@@ -33,12 +43,15 @@ public class ProfilePicture extends Resource {
     if(subject != null) {
       userId = subject.getPrincipal().toString();
     }
-    byte[] imageData;
+    InputStream imageData;
+
+    imageData = mGateway.read("files/user.png");
     try {
-      imageData = mBinaryContentManager.get(userId, BinaryContentManager.Domain.PICTURE);
-    } catch(IOException fl) {
+    } catch(Exception fl) {
+      mLogger.error(fl.getMessage());
       return Response.status(Response.Status.NOT_FOUND).build();
     }
-    return Response.ok(new ByteArrayInputStream(imageData)).build();
+
+    return Response.ok(imageData).build();
   }
 }
