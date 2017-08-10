bea94d2b0bc681c214ca0ff9b9dc918fe1a72754
kawsurilu <kawsurilu@yahoo.com>
2017-08-10 16:46:52 +0600

    ckEdior added to vendors

--- ums-webapps/ums-registrar-web/src/main/ts/controller/meeting-management/AgendaAndResolution.ts
+++ ums-webapps/ums-registrar-web/src/main/ts/controller/meeting-management/AgendaAndResolution.ts
@@ -3,25 +3,29 @@ module ums{
         id: number;
         name: string;
     }
-
+    interface IAgendaAndResolution{
+        id: number;
+        agendaNo: string;
+        agenda: string;
+        resolution: string;
+    }
     class AgendaAndResolution{
         public static $inject = ['registrarConstants', 'notify'];
         private meetingTypes: Array<IConstants> = [];
         private meetingNumbers: Array<number> = [];
         private meetingType: IConstants = null;
+        private showEditorForAgenda: boolean = false;
+        private showEditorForResolution: boolean = false;
+        private showAgendaAndResolutionDiv: boolean = false;
+        private showResolutionDiv: boolean = true;
+        private agendaAndResolutions: IAgendaAndResolution[];
 
         constructor(private registrarConstants: any,
                     private notify: Notify) {
 
             this.meetingTypes = registrarConstants.meetingTypes;
-            console.log(this.meetingTypes);
-
-            // CKEDITOR.replace('CKEditor');
-            // CKEDITOR.replace('inlineCKEditor');
-            // CKEDITOR.disableAutoInline = true;
-            // CKEDITOR.config.resize_enabled = false;
-            // CKEDITOR.instances['CKEditor'].setData("");
-            // CKEDITOR.config.removePlugins = 'elementspath';
+            CKEDITOR.replace('CKEditorForAgenda');
+            CKEDITOR.replace('CKEditorForResolution');
         }
 
         private getMeetingNumber(): void{
@@ -29,13 +33,59 @@ module ums{
                 this.notify.error("Select A Meeting Type");
             }
             else {
-                this.meetingNumbers = [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-                Utils.expandRightDiv();
+                this.meetingNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
             }
         }
 
         private getAgendaResolution(): void{
-            this.notify.error("fetching Data...");
+            this.agendaAndResolutions = [{id: 1, agendaNo: "BOT 1", agenda: "Improve Univeristy quality by using automation and automatic monitoring", resolution: ""},
+                {id: 2, agendaNo: "BOT 2", agenda: "Improve Univeristy quality by using automation and automatic monitoring", resolution: ""},
+                {id: 3, agendaNo: "BOT 3", agenda: "Improve Univeristy quality by using automation and automatic monitoring", resolution: ""},
+                {id: 4, agendaNo: "BOT 4", agenda: "Improve Univeristy quality by using automation and automatic monitoring", resolution: ""},
+                {id: 5, agendaNo: "BOT 5", agenda: "Improve Univeristy quality by using automation and automatic monitoring", resolution: ""},
+                {id: 6, agendaNo: "BOT 6", agenda: "Improve Univeristy quality by using automation and automatic monitoring", resolution: ""}];
+            Utils.expandRightDiv();
+        }
+
+        private toggleEditor(type: string, param: string): void{
+            if(type === 'agenda') {
+                if (param == "editor") {
+                    this.showEditorForAgenda = true;
+                }
+                else {
+                    this.showEditorForAgenda = false;
+                }
+            }
+            else{
+                if (param == "editor") {
+                    this.showEditorForResolution = true;
+                }
+                else {
+                    this.showEditorForResolution = false;
+                }
+            }
+        }
+
+        private toggleAgendaAndResolutionDiv(param: string): void{
+            if(param === "show"){
+                this.showAgendaAndResolutionDiv = true;
+            }
+            else{
+                this.showAgendaAndResolutionDiv = false;
+            }
+        }
+
+        private toggleResolutionDiv(param: string): void{
+            if(param === "show"){
+                this.showResolutionDiv = true;
+            }
+            else{
+                this.showResolutionDiv = false;
+            }
+        }
+
+        private addAgendaAndResolution(): void{
+
         }
     }
 
--- ums-webapps/ums-registrar-web/src/main/ts/ums-registrar.ts
+++ ums-webapps/ums-registrar-web/src/main/ts/ums-registrar.ts
@@ -113,16 +113,8 @@ module ums {
                 loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
                     return $ocLazyLoad.load({
                         files: [
-                            'vendors/select2/select2-madmin.css',
-                            'vendors/bootstrap-select/bootstrap-select.min.css',
-                            'vendors/multi-select/css/multi-select-madmin.css',
-                            'vendors/select2/select2.min.js',
-                            'vendors/bootstrap-select/bootstrap-select.min.js',
-                            'vendors/multi-select/js/jquery.multi-select.js',
-                            'vendors/bootstrap-switch/css/bootstrap-switch.css',
                             'vendors/bootstrap-datepicker/css/datepicker.css',
-                            'vendors/bootstrap-datepicker/js/bootstrap-datepicker.js',
-                            'vendors/bootstrap-switch/js/bootstrap-switch.min.js'
+                            'vendors/bootstrap-datepicker/js/bootstrap-datepicker.js'
                         ]
                     });
                 }]
@@ -137,16 +129,8 @@ module ums {
                 loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
                     return $ocLazyLoad.load({
                         files: [
-                            'vendors/select2/select2-madmin.css',
-                            'vendors/bootstrap-select/bootstrap-select.min.css',
-                            'vendors/multi-select/css/multi-select-madmin.css',
-                            'vendors/select2/select2.min.js',
-                            'vendors/bootstrap-select/bootstrap-select.min.js',
-                            'vendors/multi-select/js/jquery.multi-select.js',
-                            'vendors/bootstrap-switch/css/bootstrap-switch.css',
-                            'vendors/bootstrap-datepicker/css/datepicker.css',
-                            'vendors/bootstrap-datepicker/js/bootstrap-datepicker.js',
-                            'vendors/bootstrap-switch/js/bootstrap-switch.min.js'
+                            'vendors/ckeditor/ckeditor.js',
+                            'vendors/ckeditor/styles.js'
                         ]
                     });
                 }]
