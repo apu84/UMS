775ba1fd2074bb4ad9d673e471c8c14f176c2f81
monjurmorshed793 <monjurmorshed793@gmail.com>
2017-08-17 09:44:53 +0600

    Intial logger implemented in academic-webservices.

--- ums-webservice-academic/src/main/java/org/ums/academic/resource/GradeSubmissionResource.java
+++ ums-webservice-academic/src/main/java/org/ums/academic/resource/GradeSubmissionResource.java
@@ -6,6 +6,7 @@ import org.ums.enums.ExamType;
 import org.ums.resource.Resource;
 
 import javax.json.JsonObject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Request;
@@ -21,20 +22,20 @@ public class GradeSubmissionResource extends MutableGradeSubmissionResource {
 
   @GET
   @Path("/semester/{semester-id}/courseid/{course-id}/examtype/{exam-type}/role/{role}")
-  public JsonObject getExamGrade(final @Context Request pRequest, final @PathParam("semester-id") Integer pSemesterId,
-      final @PathParam("course-id") String pCourseId, final @PathParam("exam-type") Integer pExamTypeId,
-      final @PathParam("role") String pRequestedRole) {
+  public JsonObject getExamGrade(@Context HttpServletRequest pHttpServletRequest, final @Context Request pRequest,
+      final @PathParam("semester-id") Integer pSemesterId, final @PathParam("course-id") String pCourseId,
+      final @PathParam("exam-type") Integer pExamTypeId, final @PathParam("role") String pRequestedRole) {
     return mResourceHelper.getGradeList(pRequestedRole, pSemesterId, pCourseId, ExamType.get(pExamTypeId));
 
   }
 
   @GET
   @Path("/semester/{semester-id}/examtype/{exam-type}/dept/{dept-id}/program/{program-id}/yearsemester/{year-semester}/role/{role}/status/{status}")
-  public JsonObject getGradeSubmissionStatus(final @Context Request pRequest,
-      final @PathParam("semester-id") Integer pSemesterId, final @PathParam("exam-type") Integer pExamTypeId,
-      final @PathParam("program-id") Integer pProgramId, final @PathParam("year-semester") Integer pYearSemester,
-      final @PathParam("dept-id") String pDeptId, final @PathParam("role") String pUserRole,
-      final @PathParam("status") int pStatus) {
+  public JsonObject getGradeSubmissionStatus(@Context HttpServletRequest pHttpServletRequest,
+      final @Context Request pRequest, final @PathParam("semester-id") Integer pSemesterId,
+      final @PathParam("exam-type") Integer pExamTypeId, final @PathParam("program-id") Integer pProgramId,
+      final @PathParam("year-semester") Integer pYearSemester, final @PathParam("dept-id") String pDeptId,
+      final @PathParam("role") String pUserRole, final @PathParam("status") int pStatus) {
     return mResourceHelper.getGradeSubmissionStatus(pSemesterId, pExamTypeId, pProgramId, pYearSemester, pDeptId,
         pUserRole, pStatus);
 
@@ -42,43 +43,44 @@ public class GradeSubmissionResource extends MutableGradeSubmissionResource {
 
   @GET
   @Path("/chartdata/semester/{semester-id}/courseid/{course-id}/examtype/{exam-type}/coursetype/{course-type}")
-  public JsonObject getChartData(final @Context Request pRequest, final @PathParam("semester-id") Integer pSemesterId,
-      final @PathParam("course-id") String pCourseId, final @PathParam("exam-type") Integer pExamTypeId,
-      final @PathParam("course-type") Integer pCourseTypeId) {
+  public JsonObject getChartData(@Context HttpServletRequest pHttpServletRequest, final @Context Request pRequest,
+      final @PathParam("semester-id") Integer pSemesterId, final @PathParam("course-id") String pCourseId,
+      final @PathParam("exam-type") Integer pExamTypeId, final @PathParam("course-type") Integer pCourseTypeId) {
     return mResourceHelper.getChartData(pSemesterId, pCourseId, pExamTypeId, pCourseTypeId);
   }
 
   @GET
   @Path("/deadline/semester/{semester-id}/examType/{exam-type}/courseType/{course-type}/examDate/{exam-date}")
-  public JsonObject getGradeSubmissionDeadLine(final @Context Request pRequest,
-      final @PathParam("semester-id") Integer pSemesterId, final @PathParam("exam-type") Integer pExamType,
-      final @PathParam("course-type") int pCourseType, final @PathParam("exam-date") String pExamDate) {
+  public JsonObject getGradeSubmissionDeadLine(@Context HttpServletRequest pHttpServletRequest,
+      final @Context Request pRequest, final @PathParam("semester-id") Integer pSemesterId,
+      final @PathParam("exam-type") Integer pExamType, final @PathParam("course-type") int pCourseType,
+      final @PathParam("exam-date") String pExamDate) {
     return mResourceHelper.getGradeSubmissionDeadline(pSemesterId, ExamType.get(pExamType), pExamDate,
         CourseType.get(pCourseType), mUriInfo);
   }
 
   @GET
   @Path("/semester/{semester-id}/courseid/{course-id}/examType/{exam-type}")
-  public JsonObject getMarksSubmissionLogs(final @Context Request pRequest,
-      final @PathParam("semester-id") Integer pSemesterId, final @PathParam("course-id") String pCourseId,
-      final @PathParam("exam-type") Integer pExamType) {
+  public JsonObject getMarksSubmissionLogs(@Context HttpServletRequest pHttpServletRequest,
+      final @Context Request pRequest, final @PathParam("semester-id") Integer pSemesterId,
+      final @PathParam("course-id") String pCourseId, final @PathParam("exam-type") Integer pExamType) {
     return mResourceHelper.getMarksSubmissionLogs(pSemesterId, pCourseId, pExamType);
   }
 
   @GET
   @Path("/semester/{semester-id}/courseid/{course-id}/examType/{exam-type}/studentid/{student-id}")
-  public JsonObject getMarksLogs(final @Context Request pRequest, final @PathParam("semester-id") Integer pSemesterId,
-      final @PathParam("course-id") String pCourseId, final @PathParam("exam-type") Integer pExamType,
-      final @PathParam("student-id") String pStudentId) {
+  public JsonObject getMarksLogs(@Context HttpServletRequest pHttpServletRequest, final @Context Request pRequest,
+      final @PathParam("semester-id") Integer pSemesterId, final @PathParam("course-id") String pCourseId,
+      final @PathParam("exam-type") Integer pExamType, final @PathParam("student-id") String pStudentId) {
     return mResourceHelper.getMarksLogs(pSemesterId, pCourseId, ExamType.get(pExamType), pStudentId);
   }
 
   @GET
   @Path("/submissionstat/programtype/{program-type}/semester/{semester-id}/dept/{dept-id}/examtype/{exam-type}/status/{status}")
-  public JsonObject getMarksSubmissionStat(final @Context Request pRequest,
-      final @PathParam("program-type") Integer programType, final @PathParam("semester-id") Integer pSemesterId,
-      final @PathParam("dept-id") String pDeptId, final @PathParam("exam-type") Integer pExamType,
-      final @PathParam("status") String pStatus) throws Exception {
+  public JsonObject getMarksSubmissionStat(@Context HttpServletRequest pHttpServletRequest,
+      final @Context Request pRequest, final @PathParam("program-type") Integer programType,
+      final @PathParam("semester-id") Integer pSemesterId, final @PathParam("dept-id") String pDeptId,
+      final @PathParam("exam-type") Integer pExamType, final @PathParam("status") String pStatus) throws Exception {
     return mResourceHelper.getMarksSubmissionStat(programType, pSemesterId, pDeptId, pExamType, pStatus);
   }
 }
--- ums-webservice-academic/src/main/java/org/ums/validator/UmsLogTracerAspect.java
+++ ums-webservice-academic/src/main/java/org/ums/validator/UmsLogTracerAspect.java
@@ -1,28 +1,42 @@
 package org.ums.validator;
 
-/**
- * Created by Monjur-E-Morshed on 08-Aug-17.
- */
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
-/*
- * @Aspect
- * 
- * @Component public class UmsLogTracerAspect {
- * 
- * Logger mLogger = LoggerFactory.getLogger(UmsLogTracerAspect.class);
- * 
- * @Autowired private KafkaTemplate<String, String> mKafkaTemplate;
- * 
- * @Pointcut("within(org.ums.manager.*)") public void inWebLayer() {}
- * 
- * @After("execution(* org.ums.manager..*(..))") public void generateLog(JoinPoint pJoinPoint) {
- * 
- * System.out.println("***************"); System.out.println("logger---->" +
- * pJoinPoint.getSignature().toString()); mLogger.trace("the logger******************");
- * 
- * mKafkaTemplate.send("ums_logger", "the method accessed --> ");
- * 
- * }
- * 
- * }
- */
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class UmsLogTracerAspect {
+
+  Logger mLogger = LoggerFactory.getLogger(UmsLogTracerAspect.class);
+
+  @Autowired
+  private KafkaTemplate<String, String> mKafkaTemplate;
+
+  @Pointcut("within(org.ums.manager.*)")
+  public void inWebLayer() {}
+
+  @After("execution(* org.ums.academic.resource..*.*(..)) && args(httpServletRequest) ")
+  public void generateLog(JoinPoint pJoinPoint, HttpServletRequest httpServletRequest) {
+    HttpServletRequest mHttpServletRequest = httpServletRequest;
+
+    System.out.println("***************");
+    System.out.println("logger---->" + pJoinPoint.getSignature().toString());
+
+    System.out.println("Url: " + mHttpServletRequest.getRequestURI());
+    System.out.println("Authentication Type: " + mHttpServletRequest.getAuthType());
+    System.out.println("Ip Address: " + mHttpServletRequest.getRemoteAddr());
+
+    // mKafkaTemplate.send("ums_logger", "the method accessed --> ");
+    //
+  }
+
+}
