a5c8492958c8f98da1a2fe8f5bcecd503193c243
kawsurilu <kawsurilu@yahoo.com>
2017-09-17 11:44:52 +0600

    modify configuration in ckeditor

--- ums-webapps/ums-web-templates/src/main/webapp/iums/vendors/ckeditor/config.js
+++ ums-webapps/ums-web-templates/src/main/webapp/iums/vendors/ckeditor/config.js
@@ -9,27 +9,24 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
 // Toolbar configuration generated automatically by the editor based on config.toolbarGroups.
 config.toolbar = [
-	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: ['Print', '-', 'Templates' ] },
+	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ ] },
 	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Undo', 'Redo' ] },
 	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll' ] },
 	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'RemoveFormat' ] },
 	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
-	{ name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar' ] },
-	{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-	{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+	{ name: 'insert', items: [ 'Table', 'SpecialChar' ] },
 	{ name: 'tools', items: [ 'Maximize'] }
 ];
 
 // Toolbar groups configuration.
 config.toolbarGroups = [
-	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
 	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
 	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 	{ name: 'insert' },
-	{ name: 'styles' },
-	{ name: 'colors' },
 	{ name: 'tools' }
 ];
+
+    config.removePlugins = 'elementspath';
 };
