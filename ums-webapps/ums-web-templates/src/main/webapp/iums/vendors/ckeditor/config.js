fef0af090cb7a76f423e91c8be573d8e51cca987
kawsurilu <kawsurilu@yahoo.com>
2017-09-26 13:32:12 +0600

    add complete ckeditor

--- ums-webapps/ums-web-templates/src/main/webapp/iums/vendors/ckeditor/config.js
+++ ums-webapps/ums-web-templates/src/main/webapp/iums/vendors/ckeditor/config.js
@@ -4,29 +4,29 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
 // Toolbar configuration generated automatically by the editor based on config.toolbarGroups.
-config.toolbar = [
-	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ ] },
-	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Undo', 'Redo' ] },
-	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll' ] },
-	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'RemoveFormat' ] },
-	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
-	{ name: 'insert', items: [ 'Table', 'SpecialChar' ] },
-	{ name: 'tools', items: [ 'Maximize'] }
-];
+    config.toolbar = [
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ ] },
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Undo', 'Redo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll' ] },
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'RemoveFormat' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
+        { name: 'insert', items: [ 'Table', 'SpecialChar' ] },
+        { name: 'tools', items: [ 'Maximize'] }
+    ];
 
 // Toolbar groups configuration.
-config.toolbarGroups = [
-	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
-	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-	{ name: 'insert' },
-	{ name: 'tools' }
-];
+    config.toolbarGroups = [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ] },
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+        { name: 'insert' },
+        { name: 'tools' }
+    ];
 
     config.removePlugins = 'elementspath';
 };
