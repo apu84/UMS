c57081ae2eadcd9abf04a2776d855be2116244f6
Ifta Khirul <ifta.khirul@ipay.com.bd>
2017-10-16 01:22:42 +0600

    UnkonwnSessionException fix.

--- microservice/src/main/java/org/ums/microservice/AbstractService.java
+++ microservice/src/main/java/org/ums/microservice/AbstractService.java
@@ -3,6 +3,7 @@ package org.ums.microservice;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.ums.configuration.UMSConfiguration;
 
@@ -15,12 +16,39 @@ public abstract class AbstractService implements Service {
             .getBackendUserPassword());
 
     try {
+      ensureUserIsLoggedOut();
       subject.login(token);
       return true;
     } catch(Exception e) {
       e.printStackTrace();
     }
     return false;
+  }
+
+
+  // Logout the user fully before continuing.
+  private void ensureUserIsLoggedOut()
+  {
+    try
+    {
+      // Get the user if one is logged in.
+      Subject currentUser = SecurityUtils.getSubject();
+      if (currentUser == null)
+        return;
+
+      // Log the user out and kill their session if possible.
+      currentUser.logout();
+      Session session = currentUser.getSession(false);
+      if (session == null)
+        return;
+
+      session.stop();
+    }
+    catch (Exception e)
+    {
+      // Ignore all errors, as we're trying to silently
+      // log the user out.
+    }
   }
 
   protected abstract SecurityManager getSecurityManager();
