c4396456ece1ab10758cd666585fd26b0f71c77b
ifti24 <ifticse_kuet@hotmail.com>
2018-01-09 00:12:09 +0600

    Changed jersey client implementation a bit for testing purpose.

--- ums-webservice-core/src/main/java/org/ums/twofa/HttpClient.java
+++ ums-webservice-core/src/main/java/org/ums/twofa/HttpClient.java
@@ -6,12 +6,12 @@ import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.*;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Configuration;
 
+import org.glassfish.jersey.client.ClientConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.ums.configuration.UMSConfiguration;
@@ -25,38 +25,62 @@ public class HttpClient {
 
   public synchronized Client getClient() {
     if(mClient == null) {
-      mClient = createClient();
+      try {
+        ClientConfig clientConfig = new ClientConfig();
+        mClient = createClient(clientConfig);
+      } catch(Exception ex) {
+        ex.printStackTrace();
+      }
     }
     return mClient;
   }
 
-  private Client createClient() {
-    if(mUMSConfiguration.isDeveloperMode()) {
-      SSLContext sslcontext;
-      try {
-        sslcontext = SSLContext.getInstance("TLS");
-      } catch(NoSuchAlgorithmException e) {
-        throw new IllegalArgumentException("Couldn't get TLS instance", e);
-      }
+  /*
+   * private Client createClient() { if(mUMSConfiguration.isDeveloperMode()) { SSLContext
+   * sslcontext; try { sslcontext = SSLContext.getInstance("TLS"); } catch(NoSuchAlgorithmException
+   * e) { throw new IllegalArgumentException("Couldn't get TLS instance", e); }
+   * 
+   * try { sslcontext.init(null, new TrustManager[] {new X509TrustManager() { public void
+   * checkClientTrusted(final X509Certificate[] arg0, final String arg1) throws CertificateException
+   * {}
+   * 
+   * public void checkServerTrusted(final X509Certificate[] arg0, final String arg1) throws
+   * CertificateException {}
+   * 
+   * public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+   * 
+   * }}, new SecureRandom()); } catch(KeyManagementException e) { throw new
+   * IllegalArgumentException("Couldn't override SSL context", e); } return
+   * ClientBuilder.newBuilder().sslContext(sslcontext).hostnameVerifier((s1, s2) -> true).build(); }
+   * else { return ClientBuilder.newClient(); } }
+   */
 
-      try {
-        sslcontext.init(null, new TrustManager[] {new X509TrustManager() {
-          public void checkClientTrusted(final X509Certificate[] arg0, final String arg1) throws CertificateException {}
+  public Client createClient(Configuration config) throws NoSuchAlgorithmException, KeyManagementException {
+    SSLContext ctx = SSLContext.getInstance("SSL");
+    ctx.init(null, certs, new SecureRandom());
 
-          public void checkServerTrusted(final X509Certificate[] arg0, final String arg1) throws CertificateException {}
+    return ClientBuilder.newBuilder().withConfig(config).hostnameVerifier(new TrustAllHostNameVerifier())
+        .sslContext(ctx).build();
+  }
 
-          public X509Certificate[] getAcceptedIssuers() {
-            return new X509Certificate[0];
-          }
-
-        }}, new SecureRandom());
-      } catch(KeyManagementException e) {
-        throw new IllegalArgumentException("Couldn't override SSL context", e);
-      }
-      return ClientBuilder.newBuilder().sslContext(sslcontext).hostnameVerifier((s1, s2) -> true).build();
+  TrustManager[] certs = new TrustManager[] {new X509TrustManager() {
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+      return null;
     }
-    else {
-      return ClientBuilder.newClient();
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+  }};
+
+  public static class TrustAllHostNameVerifier implements HostnameVerifier {
+
+    public boolean verify(String hostname, SSLSession session) {
+      return true;
     }
+
   }
 }
