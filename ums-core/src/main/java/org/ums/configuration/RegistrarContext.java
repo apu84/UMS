437ef493c858bbba6e4d8fdb2b1b2a6b95ade404
KawsurMirMd <kawsurilu@yahoo.com>
2018-02-25 15:19:09 +0600

    add missing file for deptDesignationMap

--- ums-core/src/main/java/org/ums/configuration/RegistrarContext.java
+++ ums-core/src/main/java/org/ums/configuration/RegistrarContext.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.ums.cache.CacheFactory;
+import org.ums.cache.DeptDesignationMapCache;
 import org.ums.cache.DesignationRoleMapCache;
 import org.ums.cache.meeting.AgendaResolutionCache;
 import org.ums.cache.meeting.ScheduleCache;
@@ -29,9 +30,11 @@ import org.ums.employee.service.ServiceInformationManager;
 import org.ums.employee.training.PersistentTrainingInformationDao;
 import org.ums.employee.training.TrainingInformationManager;
 import org.ums.generator.IdGenerator;
+import org.ums.manager.DeptDesignationMapManager;
 import org.ums.manager.DesignationRoleMapManager;
 import org.ums.manager.meeting.AgendaResolutionManager;
 import org.ums.manager.meeting.ScheduleManager;
+import org.ums.persistent.dao.PersistentDeptDesignationMapDao;
 import org.ums.persistent.dao.PersistentDesignationRoleMapDao;
 import org.ums.persistent.dao.meeting.PersistentAgendaResolutionDao;
 import org.ums.persistent.dao.meeting.PersistentScheduleDao;
@@ -124,5 +127,12 @@ public class RegistrarContext {
     DesignationRoleMapCache designationRoleMapCache = new DesignationRoleMapCache(mCacheFactory.getCacheManager());
     designationRoleMapCache.setManager(new PersistentDesignationRoleMapDao(mTemplateFactory.getJdbcTemplate()));
     return designationRoleMapCache;
+  }
+
+  @Bean
+  DeptDesignationMapManager deptDesignationMapManager() {
+    DeptDesignationMapCache deptDesignationMapCache = new DeptDesignationMapCache(mCacheFactory.getCacheManager());
+    deptDesignationMapCache.setManager(new PersistentDeptDesignationMapDao(mTemplateFactory.getJdbcTemplate()));
+    return deptDesignationMapCache;
   }
 }
