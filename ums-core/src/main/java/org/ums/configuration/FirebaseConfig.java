2ccdf07b245ad2eb2a51a88d18305b96384e6e81
KawsurMirMd <kawsurilu@yahoo.com>
2018-04-11 12:20:27 +0600

    add service worker

--- ums-core/src/main/java/org/ums/configuration/FirebaseConfig.java
+++ ums-core/src/main/java/org/ums/configuration/FirebaseConfig.java
@@ -3,6 +3,10 @@ package org.ums.configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.WebpushConfig;
+import com.google.firebase.messaging.WebpushNotification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -10,20 +14,35 @@ import org.springframework.context.annotation.Configuration;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Configuration
 public class FirebaseConfig {
 
   public FirebaseConfig() throws IOException {
+    initializeFirebaseApp();
+  }
 
-    FileInputStream serviceAccount =
-        new FileInputStream("D:\\IUMS-Source/push-notification-demo-6b41d-firebase-adminsdk-ca6ng-bdfc7855a5.json");
+  private void initializeFirebaseApp() throws IOException {
+    FileInputStream serviceAccount = new FileInputStream("D:\\IUMS-Source/service-account.json");
 
     FirebaseOptions options =
-        new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .setDatabaseUrl("https://push-notification-demo-6b41d.firebaseio.com/").build();
+        new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
 
     FirebaseApp.initializeApp(options);
+  }
 
+  public void send() throws InterruptedException, ExecutionException {
+
+    String registrationToken =
+        "AAAAEY8u0Hc:APA91bHPqGk2ZzrRdi9t7OKAsV-ZhSM8wZpxO5qn8r2ho46Icx0YzhGT8yPwCfGD47nJQJbSFjiaWSbviGV9T-BNF7Q7tJATg6oGeZ296oOheZRJlhl8-KoetBpiGCZ6svkVTHf_RGiW";
+
+    Message message =
+        Message.builder().putData("user", "arm").putData("message", "hello firebase, this is from IUMS")
+            .setToken(registrationToken).build();
+
+    String response = FirebaseMessaging.getInstance().sendAsync(message).get();
+    System.out.println("Sent message: " + response);
   }
 }
--- ums-core/src/main/java/org/ums/services/ApprovePublicationService.java
+++ ums-core/src/main/java/org/ums/services/ApprovePublicationService.java
@@ -35,13 +35,7 @@ public class ApprovePublicationService {
   public void setNotification(String userId, Employee sender) throws IOException, ExecutionException,
       InterruptedException {
 
-    String registrationToken = "AAAAEY8u0Hc:APA91bHPqGk2ZzrRdi9t7OKAsV-ZhSM8wZpxO5qn8r2ho46Icx0YzhGT8yPwCfGD47nJQJbSFjiaWSbviGV9T-BNF7Q7tJATg6oGeZ296oOheZRJlhl8-KoetBpiGCZ6svkVTHf_RGiW";
-
-    Message message =
-        Message.builder().putData("score", "850").putData("time", "2:45").setToken(registrationToken).build();
-
-    String response = FirebaseMessaging.getInstance().sendAsync(message).get();
-    System.out.println("Successfully sent message: " + response);
+    mFirebaseConfig.send();
 
     Notifier notifier = new Notifier() {
 
