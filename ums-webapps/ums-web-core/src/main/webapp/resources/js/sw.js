5a7ba1e8f579312c28961c563ed7aa5181b64ac9
KawsurMirMd <kawsurilu@yahoo.com>
2018-04-12 07:40:04 +0600

    change gcm_sender_id

--- ums-webapps/ums-web-core/src/main/webapp/resources/js/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-
-firebase.initializeApp({
-    'messagingSenderId': '75416653943'
-});
-
-const messaging = firebase.messaging();
