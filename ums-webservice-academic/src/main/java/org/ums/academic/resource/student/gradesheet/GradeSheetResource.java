29ebdbff97feed241fd496fc041953c8b3303969
Md. Moshlehuddin Mazumder <apu@escenic.com>
2018-05-17 18:04:49 +0600

    - Gradesheet continued work

--- ums-webservice-academic/src/main/java/org/ums/academic/resource/student/gradesheet/GradeSheetResource.java
+++ ums-webservice-academic/src/main/java/org/ums/academic/resource/student/gradesheet/GradeSheetResource.java
@@ -1,14 +1,19 @@
 package org.ums.academic.resource.student.gradesheet;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.ums.domain.model.immutable.Student;
+import org.ums.domain.model.immutable.TaskStatus;
+import org.ums.manager.StudentManager;
+import org.ums.manager.TaskStatusManager;
+import org.ums.resource.Resource;
+import org.ums.services.academic.ProcessResult;
+
 import javax.json.JsonObject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Request;
-
-import org.apache.shiro.SecurityUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.ums.resource.Resource;
+import javax.ws.rs.core.Response;
 
 @Component
 @Path("/academic/gradesheet")
@@ -16,13 +21,38 @@ import org.ums.resource.Resource;
 @Consumes(Resource.MIME_TYPE_JSON)
 public class GradeSheetResource extends Resource {
   @Autowired
-  GradeSheetResourceHelper mGradeSheetResourceHelper;
+  private GradeSheetResourceHelper mGradeSheetResourceHelper;
+
+  @Autowired
+  private TaskStatusManager mTaskStatusManager;
+
+  @Autowired
+  private StudentManager mStudentManager;
 
   @GET
   @Path("/semester" + PATH_PARAM_OBJECT_ID)
-  public JsonObject getGradesheet(final @Context Request pRequest, final @PathParam("object-id") Integer pSemesterId) {
+  public Response getGradesheet(final @Context Request pRequest, final @PathParam("object-id") Integer pSemesterId) {
     // String studentId = SecurityUtils.getSubject().getPrincipal().toString();
     String studentId = "130108006";
-    return mGradeSheetResourceHelper.getGradesheet(studentId, pSemesterId, mUriInfo);
+    Student student = mStudentManager.get(studentId);
+    String taskSemesterId
+        = mTaskStatusManager.buildTaskId(student.getProgramId(), pSemesterId, ProcessResult.PROCESS_GPA_CGPA_PROMOTION);
+    String taskYearSemesterId
+        = mTaskStatusManager.buildTaskId(student.getProgramId(), pSemesterId, student.getCurrentYear(),
+        student.getCurrentAcademicSemester(), ProcessResult.PROCESS_GPA_CGPA_PROMOTION);
+
+    TaskStatus taskStatus = null;
+    if (mTaskStatusManager.exists(taskSemesterId)) {
+      taskStatus = mTaskStatusManager.get(taskSemesterId);
+    }
+    else if (mTaskStatusManager.exists(taskYearSemesterId)) {
+      taskStatus = mTaskStatusManager.get(taskYearSemesterId);
+    }
+
+    if (taskStatus != null && taskStatus.getStatus() == TaskStatus.Status.COMPLETED) {
+      return Response.ok().entity(mGradeSheetResourceHelper.getGradesheet(studentId, pSemesterId, mUriInfo)).build();
+    }
+
+    return Response.status(404).build();
   }
 }
