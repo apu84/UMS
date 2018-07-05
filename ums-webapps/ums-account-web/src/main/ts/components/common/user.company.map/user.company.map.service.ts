7d7f2555755034478202ca94147a50f6af6e7c79
monjurmorshed793 <monjurmorshed793@gmail.com>
2018-07-05 11:51:26 +0600

    - initial work done on user-company map.

--- ums-webapps/ums-account-web/src/main/ts/components/common/user.company.map/user.company.map.service.ts
+++ ums-webapps/ums-account-web/src/main/ts/components/common/user.company.map/user.company.map.service.ts
@@ -1,6 +1,14 @@
 module ums {
   export class UserCompanyMapService {
     public companyName: string;
+
+    public static $inject = ['$q', 'HttpClient'];
+
+    private url = "company"
+
+    constructor(private $q: ng.IQService, private httpClient: HttpClient) {
+
+    }
   }
 
   UMS.service("UserCompanyMapService", UserCompanyMapService);
