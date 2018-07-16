8810f1f4aa5f52746e6e1db88fd1352ee1807fb3
Md. Moshlehuddin Mazumder <apu@escenic.com>
2018-07-16 16:19:41 +0600

    - Removed unused import

--- ums-api/src/main/java/org/ums/domain/model/immutable/ApplicationCCI.java
+++ ums-api/src/main/java/org/ums/domain/model/immutable/ApplicationCCI.java
@@ -1,6 +1,5 @@
 package org.ums.domain.model.immutable;
 
-import javafx.application.Application;
 import org.ums.domain.model.common.EditType;
 import org.ums.domain.model.common.Identifier;
 import org.ums.domain.model.common.LastModifier;
