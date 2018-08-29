e5977916cc8d0d7e3cdb49baea15d4978f5950cd
KawsurMirMd <kawsurilu@yahoo.com>
2018-08-29 16:39:51 +0600

    fixed duration

--- ums-webapps/ums-web-core/src/main/ts/ems/profile-management/experience/experience.controller.ts
+++ ums-webapps/ums-web-core/src/main/ts/ems/profile-management/experience/experience.controller.ts
@@ -1,5 +1,4 @@
 module ums {
-
     class ExperienceInformation {
 
         public static $inject = ['registrarConstants',
@@ -113,43 +112,19 @@ module ums {
             return defer.promise;
         }
 
-        private calculateDifference(tabName: string, index: number, FromDate: string, ToDate: string): void {
-            if (FromDate != "" && ToDate != "") {
-                let formattedFromDate: string = moment(FromDate, "DD/MM/YYYY").format("MM/DD/YYYY");
-                let fromDate: Date = new Date(formattedFromDate);
-                let formattedToDate: string = moment(ToDate, "DD/MM/YYYY").format("MM/DD/YYYY");
-                let toDate: Date = new Date(formattedToDate);
-                if (fromDate > toDate) {
-                    this.notify.error("From date is greater than to date");
-                } else {
-                    let diffDateString = "";
-                    let year: number = 0;
-                    let month: number = 0;
-                    let day: number = 0;
-                    let timeDiff: number = (toDate.getTime() - fromDate.getTime());
-                    let diffDays: number = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-                    if (diffDays >= 365) {
-                        year = Math.floor(diffDays / 365);
-                        month = Math.floor((diffDays % 365) / 30);
-                        day = Math.floor((diffDays % 365) % 30);
-                        diffDateString = year + " year(s) " + month + " month(s) " + day + " day(s)";
-                    }
-                    else if (diffDays >= 30) {
-                        month = Math.floor((diffDays / 30));
-                        day = Math.floor(diffDays % 30);
-                        diffDateString = month + " month(s) " + day + " day(s)";
-                    }
-                    else {
-                        diffDateString = diffDays + " day(s)";
-                    }
-                    if (tabName == "experience") {
-                        this.experience[index].experienceDuration = diffDays;
-                        this.experience[index].experienceDurationString = diffDateString;
-                    }
-                }
+        private calculateDifference(form: ng.IFormController, index: number, FromDate: string, ToDate: string): void {
+            if(Utils.validateDateInput(FromDate, ToDate, 1)) {
+                this.experience[index].experienceDuration = Utils.getDateDiffInDays(FromDate, ToDate, 1);
+                this.experience[index].experienceDurationString = Utils.getFormattedDateDiff(FromDate, ToDate, 1);
+                form.$invalid = false;
+            }
+            else{
+                this.notify.error("From date is greater than to date");
+                form.$invalid = true;
             }
         }
+
     }
 
     UMS.controller("ExperienceInformation", ExperienceInformation);
