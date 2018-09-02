e33cfc516e0b9e5ed470702170b012001ac173c5
Moshlehuddin <apu@escenic.com>
2018-09-02 18:39:10 +0600

    Logstashappender now configured with tcp

--- ums-core/src/main/java/org/ums/configuration/LogstashConfiguration.java
+++ ums-core/src/main/java/org/ums/configuration/LogstashConfiguration.java
@@ -2,14 +2,15 @@ package org.ums.configuration;
 
 import ch.qos.logback.classic.AsyncAppender;
 import ch.qos.logback.classic.LoggerContext;
-import net.logstash.logback.appender.LogstashSocketAppender;
-import net.logstash.logback.stacktrace.ShortenedThrowableConverter;
+import net.logstash.logback.appender.LogstashTcpSocketAppender;
+import net.logstash.logback.encoder.LogstashEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.net.InetSocketAddress;
 
 @Configuration
 public class LogstashConfiguration {
@@ -40,24 +41,15 @@ public class LogstashConfiguration {
   public void addLogstashAppender() {
     log.info("Initializing Logstash logging");
 
-    LogstashSocketAppender logstashAppender = new LogstashSocketAppender();
+    LogstashTcpSocketAppender logstashAppender = new LogstashTcpSocketAppender();
     logstashAppender.setName("LOGSTASH");
     logstashAppender.setContext(context);
-
     String customFields = "{\"app_name\":\"" + appName + "\"}";
-
+    LogstashEncoder encoder = new LogstashEncoder();
+    encoder.setCustomFields(customFields);
+    logstashAppender.setEncoder(encoder);
     // Set the Logstash appender config from properties
-    logstashAppender.setSyslogHost(host);
-    logstashAppender.setPort(port);
-    logstashAppender.setCustomFields(customFields);
-
-    // Limit the maximum length of the forwarded stacktrace so that it won't exceed the 8KB UDP
-    // limit of logstash
-    ShortenedThrowableConverter throwableConverter = new ShortenedThrowableConverter();
-    throwableConverter.setMaxLength(7500);
-    throwableConverter.setRootCauseFirst(true);
-    logstashAppender.setThrowableConverter(throwableConverter);
-
+    logstashAppender.addDestinations(InetSocketAddress.createUnresolved(host, port));
     logstashAppender.start();
 
     // Wrap the appender in an Async appender for performance
