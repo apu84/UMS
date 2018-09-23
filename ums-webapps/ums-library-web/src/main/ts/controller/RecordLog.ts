2048a2d3f1b4eaef07d65daa2b97fe2057e4bf73
KawsurMirMd <kawsurilu@yahoo.com>
2018-09-23 12:34:27 +0600

    added employee name in modified by option

--- ums-webapps/ums-library-web/src/main/ts/controller/RecordLog.ts
+++ ums-webapps/ums-library-web/src/main/ts/controller/RecordLog.ts
@@ -11,10 +11,12 @@ module ums {
     }
 
     export class RecordLog {
-        public static $inject = ['$q', 'notify', 'recordLogService'];
+        public static $inject = ['$q', 'notify', 'recordLogService', 'employeeService'];
+
+        public static LIBRARY_DEPT_ID = "74";
 
         private mfn: string = "";
-        private modifiedBy: string = "";
+        private modifiedBy: Employee;
         private modifiedOn: string = "";
 
         private searchResult: IRecordSearchResult[] = [];
@@ -22,18 +24,25 @@ module ums {
         private itemsPerPage = 10;
         private currentPageNumber = 1;
 
+        private employees: Employee[] = [];
+
         /*private resultTable: boolean = true;*/
 
         constructor(private $q: ng.IQService,
                     private notify: Notify,
-                    private recordLogService: RecordLogService) {
+                    private recordLogService: RecordLogService,
+                    private employeeService: EmployeeService) {
 
+            this.employeeService.getEmployees(RecordLog.LIBRARY_DEPT_ID).then((employee: any) =>{
+                this.employees = employee;
+            });
             this.modifiedOn = Utils.getFormattedCurrentDate();
             this.search();
         }
 
         public search(): void{
-            this.recordLogService.getLog(this.modifiedOn, this.modifiedBy, this.mfn).then((result: IRecordSearchResult[]) =>{
+            this.recordLogService.getLog(this.modifiedOn, (this.modifiedBy == undefined || null) ? "" :
+                this.modifiedBy.id , this.mfn).then((result: IRecordSearchResult[]) =>{
                 this.searchResult = result;
                 /*this.searchResult.length == 0 ? this.setShowResultTable = false : this.setShowResultTable = true;*/
             });
