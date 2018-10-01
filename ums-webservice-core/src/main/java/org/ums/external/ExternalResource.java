a29486d942fc03e0aa90cd3d4e258dabb06a8f37
KawsurMirMd <kawsurilu@yahoo.com>
2018-10-01 12:26:51 +0600

    modified external resource

--- ums-webservice-core/src/main/java/org/ums/external/ExternalResource.java
+++ ums-webservice-core/src/main/java/org/ums/external/ExternalResource.java
@@ -1,5 +1,6 @@
 package org.ums.external;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.ums.domain.model.immutable.Employee;
@@ -9,17 +10,17 @@ import org.ums.ems.profilemanagement.personal.PersonalInformationManager;
 import org.ums.ems.profilemanagement.publication.PublicationInformation;
 import org.ums.ems.profilemanagement.publication.PublicationInformationManager;
 import org.ums.enums.common.DegreeLevel;
+import org.ums.enums.common.PublicationType;
 import org.ums.enums.common.RoleType;
 import org.ums.logs.GetLog;
 import org.ums.manager.EmployeeManager;
+import org.ums.manager.common.CountryManager;
+import org.ums.manager.common.DegreeTitleManager;
 import org.ums.resource.Resource;
 import org.ums.usermanagement.user.User;
 import org.ums.usermanagement.user.UserManager;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -50,63 +51,137 @@ public class ExternalResource extends Resource {
   @Autowired
   AcademicInformationManager mAcademicInformationManager;
 
+  @Autowired
+  CountryManager mCountryManager;
+
+  @Autowired
+  DegreeTitleManager mDegreeTitleManager;
+
   @GET
-  @GetLog(message = "Get dummy information")
+  @GetLog(message = "Get external information of all teacher")
   public JsonObject get(@Context HttpServletRequest pHttpServletRequest, final @Context Request pRequest) {
-    return get(mUriInfo);
+    return getTeachersInformation(mUriInfo);
   }
 
-  public JsonObject get(final UriInfo pUriInfo) {
-    List<User> users = mUserManager.getUsers(RoleType.get(1021));
-    List<Employee> employees = new ArrayList<>();
-    Map<String, List<PublicationInformation>> publicationListMap = new HashMap<>();
-    Map<String, List<AcademicInformation>> academicListMap = new HashMap<>();
+  private JsonObject getTeachersInformation(final UriInfo pUriInfo) {
+    List<User> users = getUsers(RoleType.TEACHER);
+    List<Employee> employees = getEmployees(users);
+    Map<String, List<AcademicInformation>> academicListMap = getAcademicListMap(users);
+    Map<String, List<PublicationInformation>> publicationListMap = getPublicationListMap(users);
+    return getJson(users, employees, publicationListMap, academicListMap);
+  }
 
+  @NotNull
+  private Map<String, List<AcademicInformation>> getAcademicListMap(List<User> users) {
+    Map<String, List<AcademicInformation>> academicListMap = new HashMap<>();
+    for(User user : users) {
+      List<AcademicInformation> academicInformations = mAcademicInformationManager.get(user.getId());
+      academicListMap.put(user.getId(), academicInformations);
+    }
+    return academicListMap;
+  }
+
+  @NotNull
+  private Map<String, List<PublicationInformation>> getPublicationListMap(List<User> users) {
+    Map<String, List<PublicationInformation>> publicationListMap = new HashMap<>();
+    for(User user : users) {
+      List<PublicationInformation> publicationInformations = mPublicationInformationManager.get(user.getId());
+      publicationListMap.put(user.getId(), publicationInformations);
+    }
+    return publicationListMap;
+  }
+
+  @NotNull
+  private List<Employee> getEmployees(List<User> users) {
+    List<Employee> employees = new ArrayList<>();
     for(User user : users) {
       Employee employee = mEmployeeManager.get(user.getId());
       employees.add(employee);
-      List<PublicationInformation> publicationInformations = new ArrayList<>();
-      publicationInformations = mPublicationInformationManager.get(user.getId());
-      publicationListMap.put(user.getId(), publicationInformations);
-      List<AcademicInformation> academicInformations = new ArrayList<>();
-      academicInformations = mAcademicInformationManager.get(user.getId());
-      academicListMap.put(user.getId(), academicInformations);
     }
-    return convertToJson(employees, publicationListMap, academicListMap);
+    return employees;
   }
 
-  private JsonObject convertToJson(List<Employee> employeeList,
+  private List<User> getUsers(RoleType roleType) {
+    return mUserManager.getUsers(roleType);
+  }
+
+  private JsonObject getJson(List<User> users, List<Employee> employeeList,
       Map<String, List<PublicationInformation>> publicationListMap,
       Map<String, List<AcademicInformation>> academicListMap) {
+
     JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
     JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
-    for(Employee employee : employeeList) {
-      JsonObjectBuilder employeeJsonBuilder = Json.createObjectBuilder();
-      employeeJsonBuilder.add("id", employee.getId());
-      employeeJsonBuilder.add("name", employee.getPersonalInformation().getFullName());
-      employeeJsonBuilder.add("gender", employee.getPersonalInformation().getGender());
-      jsonObjectBuilder.add("personal", employeeJsonBuilder);
 
-      JsonArrayBuilder academicJsonArrayBuilder = Json.createArrayBuilder();
-      for(AcademicInformation academicInformation : academicListMap.get(employee.getId())) {
-        JsonObjectBuilder academicJsonBuilder = Json.createObjectBuilder();
-        academicJsonBuilder.add("degree", DegreeLevel.get(academicInformation.getDegreeLevelId()).getLabel());
-        academicJsonBuilder.add("institution", academicInformation.getInstitute());
-        academicJsonArrayBuilder.add(academicJsonBuilder);
-      }
-      jsonObjectBuilder.add("education", academicJsonArrayBuilder);
-
-      JsonArrayBuilder publicationJsonArrayBuilder = Json.createArrayBuilder();
-      for(PublicationInformation publicationInformation : publicationListMap.get(employee.getId())) {
-        JsonObjectBuilder publicationJsonBuilder = Json.createObjectBuilder();
-        publicationJsonBuilder.add("title", publicationInformation.getTitle());
-        publicationJsonArrayBuilder.add(publicationJsonBuilder);
-      }
-      jsonObjectBuilder.add("publication", publicationJsonArrayBuilder);
-
-      jsonArrayBuilder.add(jsonObjectBuilder);
+    for(User user : users) {
+      getJson(jsonObjectBuilder, mEmployeeManager.get(user.getId()));
+      getJson(academicListMap, jsonObjectBuilder, user);
+      getJson(publicationListMap, jsonObjectBuilder, jsonArrayBuilder, user);
     }
+
     jsonObjectBuilder.add("entries", jsonArrayBuilder);
     return jsonObjectBuilder.build();
+  }
+
+  private void getJson(Map<String, List<PublicationInformation>> publicationListMap,
+      JsonObjectBuilder jsonObjectBuilder, JsonArrayBuilder jsonArrayBuilder, User user) {
+    JsonArrayBuilder publicationJsonArrayBuilder = Json.createArrayBuilder();
+    for(PublicationInformation publicationInformation : publicationListMap.get(user.getId())) {
+      JsonObjectBuilder publicationJsonBuilder = Json.createObjectBuilder();
+      publicationJsonBuilder.add("title", publicationInformation.getTitle());
+      publicationJsonBuilder.add("id", publicationInformation.getId().toString());
+      publicationJsonBuilder.add("employeeId", publicationInformation.getEmployeeId());
+      publicationJsonBuilder.add("publicationTitle", publicationInformation.getTitle());
+      publicationJsonBuilder.add("publicationInterestGenre", publicationInformation.getInterestGenre() == null ? ""
+          : publicationInformation.getInterestGenre());
+      publicationJsonBuilder.add("publisherName", publicationInformation.getPublisherName() == null ? ""
+          : publicationInformation.getPublisherName());
+      publicationJsonBuilder.add("dateOfPublication", publicationInformation.getDateOfPublication());
+      publicationJsonBuilder.add("publicationType",
+          publicationInformation.getTypeId() == 0 ? "" : PublicationType.get(publicationInformation.getTypeId())
+              .getLabel());
+      publicationJsonBuilder.add("publicationWebLink", publicationInformation.getWebLink() == null ? ""
+          : publicationInformation.getWebLink());
+      publicationJsonBuilder.add("publicationISSN", publicationInformation.getISSN() == null ? ""
+          : publicationInformation.getISSN());
+      publicationJsonBuilder.add("publicationIssue", publicationInformation.getIssue() == null ? ""
+          : publicationInformation.getIssue());
+      publicationJsonBuilder.add("publicationVolume", publicationInformation.getVolume() == null ? ""
+          : publicationInformation.getVolume());
+      publicationJsonBuilder.add("publicationJournalName", publicationInformation.getJournalName() == null ? ""
+          : publicationInformation.getJournalName());
+      if(publicationInformation.getCountryId() == 0) {
+        publicationJsonBuilder.add("location", JsonValue.NULL);
+      }
+      else {
+        publicationJsonBuilder.add("location", mCountryManager.get(publicationInformation.getCountryId()).getName());
+      }
+      publicationJsonArrayBuilder.add(publicationJsonBuilder);
+    }
+    jsonObjectBuilder.add("publication", publicationJsonArrayBuilder);
+    jsonArrayBuilder.add(jsonObjectBuilder);
+  }
+
+  private void getJson(Map<String, List<AcademicInformation>> academicListMap, JsonObjectBuilder jsonObjectBuilder,
+      User user) {
+    JsonArrayBuilder academicJsonArrayBuilder = Json.createArrayBuilder();
+    for(AcademicInformation academicInformation : academicListMap.get(user.getId())) {
+      JsonObjectBuilder academicJsonBuilder = Json.createObjectBuilder();
+      academicJsonBuilder.add("degree", DegreeLevel.get(academicInformation.getDegreeLevelId()).getLabel());
+      academicJsonBuilder.add("degreeTitle", academicInformation.getDegreeTitleId() == 0 ? "" : mDegreeTitleManager
+          .get(academicInformation.getDegreeTitleId()).getTitle());
+      academicJsonBuilder.add("institution", academicInformation.getInstitute());
+      academicJsonBuilder.add("passingYear", academicInformation.getPassingYear());
+      academicJsonArrayBuilder.add(academicJsonBuilder);
+    }
+    jsonObjectBuilder.add("education", academicJsonArrayBuilder);
+  }
+
+  private void getJson(JsonObjectBuilder jsonObjectBuilder, Employee employee) {
+    JsonObjectBuilder employeeJsonBuilder = Json.createObjectBuilder();
+    employeeJsonBuilder.add("id", employee.getId());
+    employeeJsonBuilder.add("name", employee.getPersonalInformation().getFullName());
+    employeeJsonBuilder.add("designation", employee.getDesignation().getDesignationName());
+    employeeJsonBuilder.add("department", employee.getDepartment().getShortName());
+    jsonObjectBuilder.add("employee", employeeJsonBuilder);
   }
 }
